be7f06d7d902146caa39535f102c543c9b23396e
Maxence Klein <gromaxx@gmail.com>
2019-11-29 12:47:10 +0100

    Création variableManager
    Objet qui aura en charge de répertorier les variables utiles au
    programme puis, lors de la compilation, de leur affecter une adresse
    mémoire

---
+++ variableManager.py
@@ -0,0 +1,12 @@
+from expressionParser import *
+
+class VariableManager:
+    def __init__(self): # Constructeur
+        self.list = []
+
+    def addVariable(self, nom):
+        self.list.append(nom)
+
+    def.variableRegex(self):
+        return TokenVariable.regex
+
