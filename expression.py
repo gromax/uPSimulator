091b03e306681e81f04dd4651feedfbaaa2548d7
Maxence Klein <gromaxx@gmail.com>
2019-12-03 23:43:45 +0100

    classes Node + variable Manager
    classes pour chaque noeuds d'expression afin de mieux gérer les
    commandes récursives
    
    utilisation d'un variableManager pour centraliser l'utilisation des
    variables

--- expression.py
+++ expression.py
@@ -1,113 +1,84 @@
-from expressionParser import *
 from errors import *
 
-class Expression:
-    @staticmethod
-    def strIsExpression(str):
-        '''
-        Entrée : chaine de caractères str
-        Sortie: True si cela correspond à une expression
-        '''
-        return ExpressionParser.test(str)
 
-    @classmethod
-    def __recursive_getType(cls, node):
-        '''
-        Entrée : noeud de l'arbre de l'expression
-        Sortie:
-        - 'int' si le résultat est de type 'int',
-        - 'bool' si le résultat est de type 'booléen',
-        - None en cas d'erreur
-        Parcours récursif de l'arbre
-        '''
-        if len(node) == 1:
-            # variable ou littéral
-            return 'int'
-        if len(node) == 2:
-            # opérateur unaire opérateur, opérande
-            operator, child = node
-            childType = cls.__recursive_getType(child)
-            if childType == None or (operator=='~' and childType=='bool'):
-                return None
-            elif operator == "not":
-                return 'bool'
-            else:
-                return 'int'
-        # dernier cas, opérateur binaire
-        operator, child1, child2 = node
-        child1Type = cls.__recursive_getType(child1)
-        child2Type = cls.__recursive_getType(child2)
-        if child1Type != child2Type or child1Type == None or child2Type == None:
+class UnaryNode:
+    def __init__(self,operator,operand):
+        self.__operator = operator
+        self.__operand = operand
+
+    def getType(self):
+        operandType = self.__operand.getType()
+        if operandType == None or (self.__operator == '~' and operandType=='bool'):
             return None
-        elif child1Type == 'bool' and operator in "+-*/%&|":
-            return None
-        elif child1Type == 'bool' and operator in "and;or":
+        elif self.__operator == "not":
             return 'bool'
-        elif child1Type == 'bool':
+        return 'int'
+
+    def __str__(self):
+        strOperand = str(self.__operand)
+        return self.__operator+"("+strOperand+")"
+
+class BinaryNode:
+    def __init__(self,operator,operand1, operand2):
+        self.__operator = operator
+        self.__operands = operand1, operand2
+
+    def getType(self):
+        operand1Type = self.__operands[0].getType()
+        operand2Type = self.__operands[1].getType()
+        if operand1Type != operand2Type or operand1Type == None or operand2Type == None:
+            return None
+        elif operand1Type == 'bool' and self.__operator in "+-*/%&|":
+            # cas opérateur entier agissant sur opérandes logiques
+            return None
+        elif operand1Type == 'bool' and self.__operator in "and;or":
+            # cas opérateur logique agissant sur opérandes logiques
+            return 'bool'
+        elif operand1Type == 'bool':
             # cas ==, <=...
             return None
-        elif operator in "and;or":
+        elif self.__operator in "and;or":
+            # cas opérateur logique agissant sur entiers
             return None
-        elif operator in "+-*/%&|":
+        elif self.__operator in "+-*/%&|":
+            # cas opérateur entier agissant sur entier
             return 'int'
         else:
-            # cas == <=...
+            # cas == <=... agissant sur entier
             return 'bool'
-    @classmethod
-    def __recursive_toString(cls, node):
-        '''
-        Entrée : noeud de l'arbre de l'expression
-        Sortie: chaine de caractère correspond au noeud
-        Parcours récursif de l'arbre
-        '''
-        if len(node) == 1:
-            # variable ou littéral
-            value = node[0]
-            return str(value)
-        if len(node) == 2:
-            # opérateur unaire opérateur, opérande
-            operator, child = node
-            strChild = cls.__recursive_toString(child)
-            return operator+"("+strChild+")"
-        # dernier cas, opérateur binaire
-        operator, child1, child2 = node
-        strChild1 = cls.__recursive_toString(child1)
-        strChild2 = cls.__recursive_toString(child2)
-        return "(" + strChild1 + " " + operator + " " + strChild2 + ")"
 
-    @classmethod
-    def __recursive_getVariablesNames(cls, node, variablesList):
-        '''
-        Entrée : noeud de l'arbre de l'expression, liste déjà constituée
-        Sortie : La liste
-        Parcours récursif de l'arbre
-        '''
-        if len(node) == 1:
-            # variable ou littéral
-            value = node[0]
-            if isinstance(value, str) and value not in variablesList:
-                variablesList.append(value)
-            return variablesList
-        if len(node) == 2:
-            # opérateur unaire opérateur, opérande
-            operator, child = node
-            cls.__recursive_getVariablesNames(child, variablesList)
-            return variablesList
-        # dernier cas, opérateur binaire
-        operator, child1, child2 = node
-        cls.__recursive_getVariablesNames(child1, variablesList)
-        cls.__recursive_getVariablesNames(child2, variablesList)
-        return variablesList
+    def __str__(self):
+        strOperand1 = str(self.__operands[0])
+        strOperand2 = str(self.__operands[1])
+        return "(" + strOperand1 + " " + self.__operator + " " + strOperand2 + ")"
 
-    def __init__(self, tree):
+class LitteralNode:
+    def __init__(self,value):
+        self.__value = value
+
+    def getType(self):
+        return 'int'
+
+    def __str__(self):
+        return str(self.__value)
+
+class VariableNode:
+    def __init__(self, variableObject):
+        self.__variable = variableObject
+
+    def getType(self):
+        return 'int'
+
+    def __str__(self):
+        return str(self.__variable)
+
+class Expression:
+    def __init__(self, rootNode):
         '''
         Entrée : Arbre représentant l'expression
-        Chaque noeud est un tuple de nombre d'éléments :
-        - 1: variable ou litteral,
-        - 2: opérateur unaire = string opérateur, noeud opérande,
-        - 3 opérateur binaire = string opérateur, noeud opérande 1, noeud opérande 2
+        Chaque noeud est un noeud dont le type UnaryNode, BinaryNode, LitteralNode ou VariableNode
         '''
-        self.tree = tree
+        self.__rootNode = rootNode
 
     def getType(self):
         '''
@@ -117,19 +88,10 @@ class Expression:
         - None en cas d'erreur
         Parcours récursif de l'arbre
         '''
-        return self.__recursive_getType(self.tree)
+        return self.__rootNode.getType()
 
     def __str__(self):
         '''
         Fonction de conversion de Expression en string
         '''
-        return self.__recursive_toString(self.tree)
-
-    def getVariablesNames(self):
-        '''
-        Sortie : liste des noms de variables utilisées dans l'expression
-        '''
-        variablesList = self.__recursive_getVariablesNames(self.tree, [])
-        variablesList.sort()
-        return variablesList
-
+        return str(self.__rootNode)
