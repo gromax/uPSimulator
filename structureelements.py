056dd690f0a927027e7435b4a6e90595b65ef2f2
Maxence Klein <gromaxx@gmail.com>
2019-12-15 15:16:02 +0100

    Déplacement des test structureelements.py dans le fichier lui-même

--- structureelements.py
+++ structureelements.py
@@ -344,3 +344,31 @@ class TestElement:
         return str(self.__condition)+" OUI : "+str(self.__cibleOUI)+", NON : "+str(self.__cibleNON)
 
 
+if __name__=="__main__":
+    VM = VariableManager()
+    EP = ExpressionParser(VM)
+
+    varX = VM.addVariableByName('x')
+    varY = VM.addVariableByName('y')
+    expr = EP.buildExpression('3*x+2')
+    condition = EP.buildExpression('3*x+2<4')
+
+    listeObjetsParsed = [
+      { 'type':'affectation', 'lineNumber':1, 'variable': varX, 'expression': EP.buildExpression('0') },
+      { 'type':'affectation', 'lineNumber':2, 'variable': varY, 'expression': EP.buildExpression('0') },
+      { 'type':'while', 'lineNumber':3, 'condition':EP.buildExpression('x < 10 or y < 100'), 'children':[
+        { 'type':'affectation', 'lineNumber':4, 'variable': varX, 'expression': EP.buildExpression('x + 1') },
+        { 'type':'affectation', 'lineNumber':5, 'variable': varY, 'expression': EP.buildExpression('y + x') }
+      ]},
+      { 'type':'print', 'lineNumber':6, 'expression': EP.buildExpression('y') }
+    ]
+
+    c = Container()
+    c.append(listeObjetsParsed)
+    print(c)
+    print()
+    print("Version linéarisée :")
+    print()
+    lC = c.getLinearized()
+    print(lC)
+
