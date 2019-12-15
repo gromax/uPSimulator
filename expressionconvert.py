5b45f1dcf86eed64150dd90ae44cfedade405613
gromax <gromaxx@gmail.com>
2019-12-15 12:55:58 +0100

    ajout option à l'init + attributs deviennent privés

--- expressionconvert.py
+++ expressionconvert.py
@@ -11,43 +11,46 @@ from expression import *
 
 class CompileExpressionManager:
 
-    def __init__(self):
-        self.__maxRegisters = 2
-        self.availableRegisters = list(reversed(range(self.__maxRegisters)))
-        self.registerStack = []
-        self.operationList = []
-        self.memory = -1
+    def __init__(self options={}):
+        if "maxRegisters" in options:
+            self.__maxRegisters = options["maxregisters"]
+        else:
+            self.__maxRegisters = 2
+        self.__availableRegisters = list(reversed(range(self.__maxRegisters)))
+        self.__registerStack = []
+        self.__operationList = []
+        self.__memory = -1
 
     def getAvailableRegister(self):
-        if len(self.availableRegisters) == 0:
+        if len(self.__availableRegisters) == 0:
             raise CompilationError(f"Pas de registre disponible'")
-        self.registerStack.append(self.availableRegisters.pop())
-        return self.registerStack[-1]
+        self.__registerStack.append(self.__availableRegisters.pop())
+        return self.__registerStack[-1]
 
     def freeRegister(self):
-        r = self.registerStack.pop()
-        self.availableRegisters.append(r)
+        r = self.__registerStack.pop()
+        self.__availableRegisters.append(r)
         return r
 
     def addNewOperation(self,operation):
-        self.operationList.append(operation)
+        self.__operationList.append(operation)
 
     def UALoutputIsAvailable(self):
         return 0 in self.availableRegister
 
     def storeToMemory(self,cost):
-        if cost > len(self.availableRegisters):
-            self.memory +=1
-            self.operationList.append(('registre -> memoire', self.freeRegister(),self.memory))
+        if cost > len(self.__availableRegisters):
+            self.__memory +=1
+            self.__operationList.append(('registre -> memoire', self.freeRegister(),self.__memory))
             return True
         else:
             return False
 
     def loadFromMemory(self):
-        self.operationList.append(('memoire -> registre', self.memory, self.getAvailableRegister()))
-        self.memory -=1
+        self.__operationList.append(('memoire -> registre', self.__memory, self.getAvailableRegister()))
+        self.__memory -= 1
 
     def compileExpression(self, expression):
         expression.calcCompile(self)
-        return self.operationList
+        return self.__operationList
 
