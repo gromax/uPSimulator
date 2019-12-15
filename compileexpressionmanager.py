bb45542bfef01fb6196d2c49846112a419a67486
Maxence Klein <gromaxx@gmail.com>
2019-12-15 21:12:17 +0100

    modifications variées...
    - ajout option pour autoriser litteral dans une commande
    - gestion auto d'un mouvement vers la mémoire
    - fonction adhoc pour libérer spécifiquement le registre 0 (si UAL doit
    sortir sur 0)
    - fonctions pushBinary, pushUnary pour gérer les mouvements lors de
    l'ajout d'une opération
    - mémoire stockée dans la pile des registre sous forme d'un -1
    - gestion des mouvements vers/depuis mémoire modifié

--- compileexpressionmanager.py
+++ compileexpressionmanager.py
@@ -18,6 +18,11 @@ class CompileExpressionManager:
             self.__freeUALOuptut = options["freeUALOutput"]
         else:
             self.__freeUALOuptut = True
+        if "litteralInCommand" in options:
+            self.litteralInCommand = options["litteralInCommand"]
+        else:
+            self.litteralInCommand = False
+        assert self.__maxRegisters > 1
         self.__availableRegisters = list(reversed(range(self.__maxRegisters)))
         self.__registerStack = []
         self.__operationList = []
@@ -41,44 +46,108 @@ class CompileExpressionManager:
         if len(self.__registerStack) == 0:
             raise CompilationError("Aucun registre à libérer.")
         register = self.__registerStack.pop()
+        if register == -1:
+            self.moveMemoryToFreeRegister()
+            register = self.__registerStack.pop()
         self.__availableRegisters.append(register)
         return register
+
+    def freeZeroRegister(self):
+        if not 0 in self.__registerStack:
+            raise CompilationError("Registre 0 n'est pas occupé.")
+        index0 = self.__registerStack.index(0)
+        del self.__registerStack[index0]
 
     def addNewOperation(self,operation):
         self.__operationList.append(operation)
 
+    def pushBinaryOperator(self, operator, directOrder):
+        if len(self.__registerStack) < 2:
+            raise CompilationError(f"Pas assez d'opérande pour {operator} dans la pile.")
+        '''
+         Attention ici !!!
+         On ne doit pas libérer rLastCalc avant d'avoir traiter rFirstCalc.
+         L'opération rLastCalc = freeRegister() ferait comme si rLastCalc était libre pour les éventuelles
+         manipulations de rFirstCalc. Ce n'est pas le cas. Les deux registres ne sont libérés qu'au moment de l'opération.
+        '''
+        rLastCalc = self.__registerStack[-1]
+        if rLastCalc < 0:
+            self.__registerStack.pop()
+            rLastCalc = self.moveMemoryToFreeRegister()
+        rFirstCalc = self.__registerStack[-2]
+        if rFirstCalc < 0:
+            del self.__registerStack[-2]
+            rFirstCalc = self.moveMemoryToFreeRegister()
+            '''
+            rLastCalc et rFirstCalc peuvent se retrouver dans le désordre dans la pile
+            mais c'est sans importance
+            '''
+        self.freeRegister()
+        self.freeRegister()
+        registreDestination = self.getAvailableRegister()
+
+        if directOrder:
+            operation = (operator, registreDestination, rFirstCalc, rLastCalc)
+        else:
+            operation = (operator, registreDestination, rLastCalc, rFirstCalc)
+        self.addNewOperation(operation)
+
+    def pushBinaryOperatorWithLitteral(self, operator, litteral):
+        if len(self.__registerStack) < 1:
+            raise CompilationError(f"Pas assez d'opérande pour {operator} dans la pile.")
+        registreOperand = self.freeRegister()
+        registreDestination = self.getAvailableRegister()
+        operation = (operator, registreDestination, registreOperand, litteral)
+        self.addNewOperation(operation)
+
+    def pushUnaryOperator(self, operator):
+        registreOperand = self.freeRegister()
+        registreDestination = self.getAvailableRegister()
+        operation = (operator, registreDestination, registreOperand)
+        self.addNewOperation(operation)
+
     def UALoutputIsAvailable(self):
         return self.__freeUALOuptut or 0 in self.__availableRegisters
 
-    def storeToMemory(self,cost):
-        if cost <= len(self.__availableRegisters):
-            return False
+    def moveRegisterToMemory(self,sourceRegister):
+        assert 0 <= sourceRegister < self.__maxRegisters
         self.__memoryStackLastIndex +=1
-        sourceRegister = self.freeRegister()
         operation = ('registre -> memoire', sourceRegister, self.__memoryStackLastIndex)
         self.__operationList.append(operation)
         if self.__memoryStackLastIndex > self.__memoryStackMaxIndex:
             self.__memoryStackMaxIndex = self.__memoryStackLastIndex
-        return True
+        self.__registerStack.append(-1)
 
-    def loadFromMemory(self):
+    def moveMemoryToFreeRegister(self):
         if self.__memoryStackLastIndex < 0:
             raise CompilationError("Pas de données en mémoire disponible")
         destinationRegister = self.getAvailableRegister()
         operation = ('memoire -> registre', self.__memoryStackLastIndex, destinationRegister)
         self.__operationList.append(operation)
         self.__memoryStackLastIndex -= 1
+        return destinationRegister
+
+    def getNeededRegisterSpace(self, cost, needUAL):
+        if needUAL and not self.UALoutputIsAvailable():
+            if cost > len(self.__availableRegisters):
+                self.freeZeroRegister()
+                self.moveRegisterToMemory(0)
+            else:
+                registerTemp = self.getAvailableRegister()
+                self.freeZeroRegister()
+                operation = ('registre -> registre', 0, registerTemp)
+                self.__operationList.append(operation)
+        while cost > len(self.__availableRegisters) and len(self.__availableRegisters) < self.__maxRegisters:
+            registerToStore = self.freeRegister()
+            self.moveRegisterToMemory(registerToStore)
+
+    def stringMemoryUsage(self):
+        strAvailableRegisters = ", ".join(["r"+str(it) for it in self.__availableRegisters])
+        strStackedRegisters = ", ".join([str(it) for it in self.__registerStack])
+        return f"available = [{strAvailableRegisters}] ; stacked = [{strStackedRegisters}]"
 
     def __str__ (self):
-        if self.__memoryStackLastIndex >=0:
-            output = f"memory stack -> {self.__memoryStackLastIndex}\n"
-        else:
-            output = "no memory stacked\n"
-        if self.availableRegisterExists():
-            strAvailableRegisters = ", ".join(["r"+str(item) for item in self.__availableRegisters])
-            output += f"availables registers : {strAvailableRegisters}\n"
-        else:
-            output += "no available register\n"
+        output = self.stringMemoryUsage()+"\n"
         for item in self.__operationList:
             strItem = ", ".join([str(subItem) for subItem in item])
             output += strItem+"\n"
