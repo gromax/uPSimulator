2a295adb2e72cfde06388fcbdec15bf3512c1e06
gromax <gromaxx@gmail.com>
2019-12-29 11:53:42 +0100

    ParseError plus approprié et déjà existant pour l'ensemble des erreurs de Parse

--- errors.py
+++ errors.py
@@ -29,20 +29,3 @@ class ParseError(Exception):
         # Now for your custom code...
         self.errors = errors
 
-class LineError(Exception):
-    def __init__(self, message, errors = None):
-
-        # Call the base class constructor with the parameters it needs
-        super().__init__(message)
-
-        # Now for your custom code...
-        self.errors = errors
-
-class CodeError(Exception):
-    def __init__(self, message, errors = None):
-
-        # Call the base class constructor with the parameters it needs
-        super().__init__(message)
-
-        # Now for your custom code...
-        self.errors = errors
