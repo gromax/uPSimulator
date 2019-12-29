9bd6640f660d263fdb9b48204a40f9e9bde08912
Maxence Klein <gromaxx@gmail.com>
2019-12-29 21:43:17 +0100

    ajout AttributeError pour un modèle de uP invalide

--- errors.py
+++ errors.py
@@ -29,3 +29,11 @@ class ParseError(Exception):
         # Now for your custom code...
         self.errors = errors
 
+class AttributesError(Exception):
+    def __init__(self, message, errors = None):
+
+        # Call the base class constructor with the parameters it needs
+        super().__init__(message)
+
+        # Now for your custom code...
+        self.errors = errors
