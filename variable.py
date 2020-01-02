53fee1a9f09071056c19356d3875d3416bde044f
Maxence Klein <gromaxx@gmail.com>
2020-01-02 14:20:19 +0100

    Nouveau : Variable dans fichier à part

---
+++ variable.py
@@ -0,0 +1,20 @@
+'''
+objet contenant une variable mémoire
+'''
+
+class Variable:
+    def __init__(self, nom, value = 0):
+        self.__nom = nom
+        self.__value = int(value)
+
+    def getName(self):
+        return self.__nom
+
+    def getValue(self):
+        return self.__value
+
+    def __str__(self):
+        return "@"+self.__nom
+
+    def isLitteral(self):
+        return False
