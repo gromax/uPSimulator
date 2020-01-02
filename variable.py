705fdecf1badb8c70c353f83cefbef8f15db8dea
Maxence Klein <gromaxx@gmail.com>
2020-01-02 23:02:17 +0100

    Valeur dans Variable plus nécessaire

--- variable.py
+++ variable.py
@@ -3,15 +3,11 @@ objet contenant une variable mémoire
 '''
 
 class Variable:
-    def __init__(self, nom, value = 0):
+    def __init__(self, nom):
         self.__nom = nom
-        self.__value = int(value)
 
     def getName(self):
         return self.__nom
-
-    def getValue(self):
-        return self.__value
 
     def __str__(self):
         return "@"+self.__nom
