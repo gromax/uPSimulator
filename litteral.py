9b1c58185d22038d8aa986d4e33a63faf6c38c4f
gromax <gromaxx@gmail.com>
2020-01-09 15:02:10 +0100

    Ajout de la possibilité d'une value None
    pour le cas d'un litteral pointant sur la ligne suivante

--- litteral.py
+++ litteral.py
@@ -5,23 +5,35 @@ Objet contenant un littéral
 class Litteral:
     def __init__(self, value):
         '''
-        value = int
+        value = int ou None
+        Si None, signifie que la valeur est mise au max
+        ce qui arrive quand on fait pointer vers la ligne suivante
         '''
-        assert isinstance(value,int)
+        assert value == None or isinstance(value,int)
         self.__value = value
 
     def clone(self):
         return Litteral(self.__value)
 
+    def negClone(self):
+        if self.__value == None:
+            return Litteral(None)
+        return Litteral(-self.__value)
+
     def getBinary(self, wordSize):
+        if self.__value == None:
+            return "1"*wordSize
         if self.__value < 0:
-            valuToCode = (~self.__value + 1) & (2**wordSize - 1)
+            # utilise le CA2
+            valueToCode = (~(-self.__value) + 1) & (2**wordSize - 1)
         else:
             valueToCode = self.__value
         return format(valueToCode, '0'+str(wordSize)+'b')
 
     def isBetween(self, minValue, maxValue):
-        return minValue <= self.__value <= maxValue
+        return self.__value == None or minValue <= self.__value <= maxValue
 
     def __str__(self):
+        if self.__value == None:
+            return "#Next line"
         return "#"+str(self.__value)
