6040272ba7e535c13717d5a00eaf80d8353ae908
Maxence Klein <gromaxx@gmail.com>
2020-01-22 14:22:47 +0100

    Ajoute une possibilité de value dans Variable

--- variable.py
+++ variable.py
@@ -3,8 +3,9 @@ objet contenant une variable mémoire
 '''
 
 class Variable:
-    def __init__(self, nom):
+    def __init__(self, nom, value = 0):
         self.__nom = nom
+        self.__value = value
 
     def getName(self):
         return self.__nom
@@ -17,7 +18,11 @@ class Variable:
 
     def getBinary(self, wordSize):
         '''
-        fonction renvoyant un mot plein de 0
-        utile pour simplifier la production du binaire dans assembleurcontainer.py
+        Retourne chaîne de caractère représentant le code CA2 de self.__value
         '''
-        return "0"*wordSize
+        if self.__value < 0:
+            # utilise le CA2
+            valueToCode = (~(-self.__value) + 1) & (2**wordSize - 1)
+        else:
+            valueToCode = self.__value
+        return format(valueToCode, '0'+str(wordSize)+'b')
