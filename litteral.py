7854b39ea2b023b833061f3c8a13a9fa63a617db
Maxence Klein <gromaxx@gmail.com>
2020-01-22 16:46:19 +0100

    Suppression de la possibilité de mettre None comme value de Litteral

--- litteral.py
+++ litteral.py
@@ -5,12 +5,10 @@ Objet contenant un littéral
 class Litteral:
     def __init__(self, value, big=False):
         '''
-        value = int ou None
-        Si None, signifie que la valeur est mise au max
-        ce qui arrive quand on fait pointer vers la ligne suivante
+        value = int
         big = True si au moment de la compilation on a décidé coder le littéral sur une pleine ligne
         '''
-        assert value == None or isinstance(value,int)
+        assert isinstance(value,int)
         self.__value = value
         self.__big = (big == True)
 
@@ -18,8 +16,6 @@ class Litteral:
         return Litteral(self.__value, self.__big)
 
     def negClone(self):
-        if self.__value == None:
-            return Litteral(None)
         return Litteral(-self.__value, self.__big)
 
     def setBig(self):
@@ -37,8 +33,6 @@ class Litteral:
         Retourne la version binaire du littéral
         S'il est trop grand, retourne une chaîne de 1
         '''
-        if self.__value == None:
-            return "1"*wordSize
         if self.__value < 0:
             # utilise le CA2
             valueToCode = (~(-self.__value) + 1) & (2**wordSize - 1)
@@ -50,14 +44,10 @@ class Litteral:
         return outStr
 
     def isBetween(self, minValue, maxValue):
-        return self.__value == None or minValue <= self.__value <= maxValue
+        return minValue <= self.__value <= maxValue
 
     def getValue(self):
-        if self.__value == None:
-            return 0
         return self.__value
 
     def __str__(self):
-        if self.__value == None:
-            return "#Next line"
         return "#"+str(self.__value)
