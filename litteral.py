3fd52a36d8a5f592c0b63db2f967adaeb8de0f59
Maxence Klein <gromaxx@gmail.com>
2020-01-24 20:34:38 +0100

    ajout commentaires type sphinx

--- litteral.py
+++ litteral.py
@@ -1,35 +1,43 @@
 """
 .. module:: litteral
-   :platform: Unix, Windows
    :synopsis: définition d'un objet contenant une valeur littérale
 """
 
 class Litteral:
     def __init__(self, value: int):
-        '''
-        value = int
-        '''
+        """Constructeur de la classe
+
+        :param value: valeur du littéral
+        :type value: int
+        """
         assert isinstance(value,int)
         self.__value = value
 
-    def clone(self) -> 'Litteral':
-        return Litteral(self.__value)
-
     def negClone(self) -> 'Litteral':
+        """Produit un clone du littéral avec valeur opposée
+
+        :return: clone du littéral avec valeur opposée
+        :rtype: Litteral
+
+        :Example:
+
+        >>> Litteral(8).negClone().getValue()
+        -8
+        """
+
         return Litteral(-self.__value)
 
     def isBetween(self, minValue:int, maxValue:int) -> bool:
         """Retourne True si la valeur courante est comprise entre minValue et maxValue.
-        Args:
-           minValue (int): valeur minimum
-           maxValue (int): valeur maximum
 
-        Returns:
-           bool.  The return code::
+        :param minValue: valeur minimum
+        :type minValue: int
+        :param maxValue: valeur maximum
+        :type maxValue: int
+        :return: Vrai si la valeur du littéral est compris entre minValue et maxValue, inclus
+        :rtype: bool
 
-              True -- La valeur est dans [minValue ; maxValue]
-              False -- La valeur n'est pas dans [minValue ; maxValue]
-
+        :Example:
         >>> Litteral(8).isBetween(4,12)
         True
 
@@ -41,7 +49,33 @@ class Litteral:
         return minValue <= self.__value <= maxValue
 
     def getValue(self) -> int:
+        """Retourne la valeur du littéral
+
+        :return: valeur du littéral
+        :rtype: int
+
+        :Example:
+
+        >>> Litteral(8).getValue()
+        8
+
+        >>> Litteral(-15).getValue()
+        -15
+
+        """
+
         return self.__value
 
     def __str__(self) -> str:
+        """Transtypage -> str. Affiche le littéral préfixé par #
+
+        :return: # + valeur du littéral
+        :rtype: str
+
+        :Example:
+
+        >>> str(Litteral(8))
+        '#8'
+
+        """
         return "#"+str(self.__value)
--- variable.py
+++ variable.py
@@ -1,27 +1,76 @@
-'''
-objet contenant une variable mémoire
-'''
+"""
+.. module:: variable
+   :synopsis: définition d'un objet contenant une variable mémoire
+"""
 
 from errors import *
 
 class Variable:
     def __init__(self, nom:str, value:int = 0):
+        """Constructeur de la classe
+
+        :param nom: nom de la variable
+        :type nom: str
+        :param value: valeur initiale. 0 par défaut
+        :type value: int
+        """
         self.__nom = nom
         self.__value = value
 
     def getName(self) -> str:
+        """Retourne le nom de la variable : attribut __name
+
+        :return: nom de la variable
+        :rtype: str
+
+        :Example:
+
+        >>> Variable("x").getName()
+        'x'
+
+        .. warning:: Il est possible que l'on crée plusieurs variables pour un même nom
+        """
+
         return self.__nom
 
-    def clone(self) -> 'Variable':
-        return Variable(self.__nom)
-
     def __str__(self) -> 'str':
+        """Transtypage -> str. Affiche le nom de la variable préfixé par @
+
+        :return: @ + nom de la variable
+        :rtype: str
+
+        :Example:
+
+        >>> str(Variable("x"))
+        '@x'
+
+        """
+
         return "@"+self.__nom
 
-    def getBinary(self, wordSize:int) -> 'str':
-        '''
-        Retourne chaîne de caractère représentant le code CA2 de self.__value
-        '''
+    def getValueBinary(self, wordSize:int) -> 'str':
+        """Retourne chaîne de caractère représentant le code CA2 de self.__value,
+        pour un mot de taille wordSize bits
+
+        :param wordSize: taille du mot binaire
+        :type wordSize: int
+        :return: code CA2 de la valeur initiale de la variable, sur wordSize bits
+        :rtype: str
+
+        :Example:
+
+        >>> Variable("x",45).getValueBinary(8)
+        '00101101'
+
+        >>> Variable("x",-45).getValueBinary(8)
+        '11010011'
+
+        >>> Variable("x",1000).getValueBinary(8)
+        Traceback (most recent call last):
+        ...
+        errors.CompilationError: @x : Variable de valeur trop grande !
+        """
+
         if self.__value < 0:
             # utilise le CA2
             valueToCode = (~(-self.__value) + 1) & (2**wordSize - 1)
@@ -33,4 +82,18 @@ class Variable:
         return outStr
 
     def getValue(self) -> int:
+        """Retourne la valeur initiale de la variable
+
+        :return: valeur initiale de la variable
+        :rtype: int
+
+        :Example:
+
+        >>> Variable("x").getValue()
+        0
+
+        >>> Variable("x",15).getValue()
+        15
+
+        """
         return self.__value
