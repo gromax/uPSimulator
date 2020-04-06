a68b2683746517f8af14794d24cafea5a58346dd
Maxence Klein <gromaxx@gmail.com>
2020-04-06 18:53:02 +0200

    Commande de menu déplacées dans la fenêtre principale
    Ajout de divers commandes :
    - run 1x
    - run 10x
    - run 100x
    - reinit
    - pause

--- graphic.py
+++ graphic.py
@@ -12,17 +12,82 @@ from executeur import Executeur
 from widgets import InputCodeWidget, SimulationWidget
 from errors import *
 
+
+
+
+
 class InputCodeWindow:
-    __inputCodeFrame = None
-    __simulationFrame = None
+    EXEMPLES = [
+        ("exemple 1", "example.code"),
+        ("exemple 2", "example2.code"),
+        ("exemple 3", "example3.code")
+    ]
+    _runningSpeed = 0
     def __init__(self):
         root = Tk()
         root.title('Simulation de processeur')
-        self.__root = root
-        inputCodeFrame = InputCodeWidget(root, self.doCompile)
-        inputCodeFrame.pack()
+        self._root = root
 
-    def doCompile(self, textCode, codeFrame = None):
+        menu = Menu(root)
+        root['menu'] = menu
+        menu.add_command(label='Compilation', command=self.__editModeCompile)
+        menu.add_command(label='Effacer', command=self.__clearProgramInput)
+
+        sousMenuExemples = Menu(menu)
+        menu.add_cascade(label='Exemples', menu=sousMenuExemples)
+        for nomExemple, nomFichier in self.EXEMPLES:
+            self.__addExemple(sousMenuExemples, nomExemple, nomFichier)
+
+        sousMenuRun = Menu(menu)
+        menu.add_cascade(label="Run", menu = sousMenuRun)
+        sousMenuRun.add_command(label="step", command=self.__step)
+        sousMenuRun.add_command(label="Run x1", command=self.__run_v1)
+        sousMenuRun.add_command(label="Run x10", command=self.__run_v10)
+        sousMenuRun.add_command(label="Run x100", command=self.__run_v100)
+        sousMenuRun.add_command(label="Pause", command=self.__pause)
+        sousMenuRun.add_command(label="Réinit", command=self.__reinitSim)
+
+
+        self._currentWidget = InputCodeWidget(root)
+        self._currentWidget.pack()
+
+    def inEditMode(self):
+        return isinstance(self._currentWidget, InputCodeWidget)
+
+    def __addExemple(self, sousMenu, nomExemple, nomFichier):
+        sousMenu.add_command(label=nomExemple, command=lambda:self.__loadFile(nomFichier))
+
+    def __loadFile(self, fileName):
+        try:
+            with open(fileName, 'r') as file:
+                fileText = file.read()
+        except Exception as e:
+            self.writeMessage("Impossible d'ouvrir le fichier exemple '{}'\n{}".format(fileName, e))
+        else:
+            if self.inEditMode():
+                self._currentWidget.writeProgramInput(fileText)
+            else:
+                self.__doCompile(fileText)
+
+    def __clearProgramInput(self):
+        if self.inEditMode():
+            self._currentWidget.clearProgramInput()
+
+    def __goEditMode(self):
+        pass
+
+    def __goSimMode(self, executeur, textCode, asm):
+        self._currentWidget.destroy()
+        self._currentWidget = SimulationWidget(self._root, executeur, textCode, asm)
+        self._currentWidget.pack()
+
+    def __editModeCompile(self):
+        if self.inEditMode():
+            self._currentWidget.writeMessage("Compilation...")
+            textCode = self._currentWidget.getCode()
+            self.__doCompile(textCode)
+
+    def __doCompile(self, textCode):
         engine = ProcessorEngine()
         try :
             cp = CodeParser(code = textCode)
@@ -36,25 +101,57 @@ class InputCodeWindow:
                 errorMessage = "[{}] {}".format(e.errors["lineNumber"], e)
             else :
                 errorMessage = str(e)
-            if codeFrame != None:
-                codeFrame.writeMessage(errorMessage)
+            if self.inEditMode():
+                self._currentWidget.writeMessage(errorMessage)
                 if "lineNumber" in e.errors:
-                    codeFrame.highlightLine(e.errors["lineNumber"])
+                    self._currentWidget.highlightLine(e.errors["lineNumber"])
             else:
                 print(errorMessage)
         except Exception as e:
-            if codeFrame != None:
-                codeFrame.writeMessage(e)
+            if self.inEditMode() != None:
+                self._currentWidget.writeMessage(e)
             else:
                 print(e)
         else :
-            if isinstance(codeFrame, InputCodeWidget):
-                codeFrame.destroy()
-            simFrame = SimulationWidget(self.__root, executeur, textCode, asm)
-            simFrame.pack()
+            self.__goSimMode(executeur, textCode, asm)
+
+    def __run_v1(self):
+        self._runningSpeed = 1000
+        self.__step()
+
+    def __run_v10(self):
+        self._runningSpeed = 100
+        self.__step()
+
+    def __run_v100(self):
+        self._runningSpeed = 10
+        self.__step()
+
+    def __pause(self):
+        self._runningSpeed = 0
+
+    def __reinitSim(self):
+        if not self.inEditMode():
+            engine = ProcessorEngine()
+            try :
+                asm = self._currentWidget.asm
+                executeur = Executeur(engine,asm.getDecimal())
+            except Exception as e:
+                self._currentWidget.addMessage(str(e))
+            else :
+                textCode = self._currentWidget.textCode
+                self.__goSimMode(executeur, textCode, asm)
+
+    def __step(self):
+        if self.inEditMode():
+            self.__editModeCompile()
+        if not self.inEditMode():
+            self._currentWidget.stepRun()
+            if self._runningSpeed > 0:
+                self._root.after(self._runningSpeed, self.__step)
 
     def show(self):
-        self.__root.mainloop()
+        self._root.mainloop()
 
 
 
