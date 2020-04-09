6525b30cd98af0c601829f633a9a6cd3e7e0ef36
Maxence Klein <gromaxx@gmail.com>
2020-04-09 18:55:10 +0200

    Simplification...
    La linéarisation de la structure est de la responsabilité de
    structuresnodes.
    
    compile se contente alors de demander la linéarisation à la liste
    fournie.
    
    toutes les fonctions consistant à tracker les labels inutiles, saut
    inutiles... deviennent inutiles

--- compilemanager.py
+++ compilemanager.py
@@ -11,49 +11,28 @@ from compileexpressionmanager import CompileExpressionManager
 from processorengine import ProcessorEngine
 
 class CompilationManager:
-    __engine:ProcessorEngine
-    __linearList:List[StructureNode]
-    __asm:AssembleurContainer
-    def __init__(self, engine:ProcessorEngine, structureNodeList:List[StructureNode]):
+    _engine:ProcessorEngine
+    _linearList:StructureNodeList
+    _asm:AssembleurContainer
+    def __init__(self, engine:ProcessorEngine, listOfStructureNodes:List[StructureNode]):
         """Constructeur
 
         :param engine: objet décrivant le modèle de processeur
         :type engine: ProcessorEngine
-        :param structureNodeList: liste de noeuds décrivant le programme à compiler
-        :type structureNodeList: list(StructureNode)
+        :param listOfStructureNodes: liste de noeuds décrivant le programme à compiler
+        :type listOfStructureNodes: list[StructureNode]
 
         .. note::
 
         Fait immédiatement la compilation et crée un objet assembleur pour stocker le résultat.
         la liste fournie n'est pas stockée, on en produit une forme linéaire (while et if transformé) aussitôt.
         """
-        self.__engine = engine
-        self.__asm = AssembleurContainer(self.__engine)
-        self.__linearList = self.__compile(structureNodeList)
+        self._engine = engine
+        self._asm = AssembleurContainer(self._engine)
+        self._linearList = StructureNodeList(listOfStructureNodes)
+        comparaisonSymbolsAvailables = self._engine.getComparaisonSymbolsAvailables()
+        self._linearList.linearize(comparaisonSymbolsAvailables)
         self.__compileASM()
-
-    def __compile(self, structureNodeList:List[StructureNode]) -> List[StructureNode]:
-        """Linéarise le programme fourni. Les while et if sont transformés en itilisant des sauts conditionnels ou non.
-
-        :param structureNodeList: liste de noeuds décrivant le programme à compiler
-        :type structureNodeList: list[StructureNode]
-        :return: programme sous sa forme linéaire.
-        :rtype: list[StructureNode]
-
-        .. note::
-
-        La création des sauts conditionnels suppose de connaître les possibilités du processeur concernant les tests disponibles. On demande donc au modèle de processeur les tests disponibles.
-        """
-        comparaisonSymbolsAvailables = self.__engine.getComparaisonSymbolsAvailables()
-        for node in structureNodeList:
-            assert isinstance(node, StructureNode)
-        linearList = []
-        for node in structureNodeList:
-            linearForNode = node.getLinearStructureList(comparaisonSymbolsAvailables)
-            linearList.extend(linearForNode)
-        self.__cleanList(linearList)
-        self.__plugLabels(linearList)
-        return linearList
 
     def __str__(self) -> str:
         """Transtypage -> str
@@ -61,96 +40,7 @@ class CompilationManager:
         :return: programme linéarisé en version texte
         :rtype: str
         """
-        return "\n".join([str(item) for item in self.__linearList])
-
-    def __getLabels(self, linearList:List[StructureNode]) -> Dict[LabelNode,List[JumpNode]]:
-        """Fournit un dictionnaire donnant toutes les étiquettes jointes à tous les sauts pointant sur elles.
-
-        :param linearList: liste de noeuds de la version linéaire
-        :type linearList: list[StructureNode]
-        :return: dictionnaire de forme étiquette -> liste des sauts pointant sur cette étiquette
-        :rtype: dict[LabelNode,list[JumpNode]]
-        """
-        labels:Dict[LabelNode,List[JumpNode]] = {}
-        for node in linearList:
-            if isinstance(node,LabelNode):
-                labels[node] = []
-        # association jump -> label
-        for node in linearList:
-            if isinstance(node,JumpNode):
-                cible = node.cible
-                if not cible in labels:
-                    raise CompilationError("Saut vers label inconnu : "+str(cible), {"lineNumber":node.lineNumber})
-                labels[cible].append(node)
-        return labels
-
-    def __delJumpToNextLine(self, linearList:List[StructureNode]) -> bool:
-        """Efface les sauts, conditionnels ou non, consistant à sauter à la ligne suivante.
-        En effet, le déroulement normal du programme est de passer à la ligne suivante, de tels
-        sauts sont donc superflus.
-        :param linearList: liste de noeuds de la version linéaire
-        :type linearList: list[StructureNode]
-        :return: vrai si un changement a été effectué
-        :rtype: bool
-        """
-        flagActionDone = False
-        if len(linearList) < 2:
-            return False
-        index = 0
-        while index < len(linearList)-1:
-            currentNode = linearList[index]
-            nextNode = linearList[index+1]
-            if isinstance(currentNode,JumpNode) and currentNode.cible == nextNode:
-                linearList.pop(index)
-                flagActionDone = True
-            else:
-                index += 1
-        return flagActionDone
-
-    def __delNotUseLabel(self, linearList:List[StructureNode]) -> bool:
-        """Efface les étiquettes qui ne sont jamais utilisées. ie : qui ne sont la cible d'aucun saut.
-        :param linearList: liste de noeuds de la version linéaire
-        :type linearList: list[StructureNode]
-        :return: vrai si un changement a été fait
-        :rtype: bool
-        """
-        flagActionDone = False
-        labels = self.__getLabels(linearList)
-        for label, listeJumps in labels.items():
-            if len(listeJumps) == 0:
-                flagActionDone = True
-                indexToDel = linearList.index(label)
-                linearList.pop(indexToDel)
-        return flagActionDone
-
-    def __cleanList(self, linearList:List[StructureNode]) -> None:
-        """Nettoie la liste linéaire en
-        supprimant les étiquettes non utilisées,
-        supprimant les sauts pointant vers la ligne suivante.
-
-        :param linearList: liste de noeuds de la version linéaire
-        :type linearList: list[StructureNode]
-        :return: pas de retour, liste modifiée en place
-        """
-        goOnFlag = True
-        while goOnFlag:
-            goOnFlag = False
-            goOnFlag = self.__delNotUseLabel(linearList) or goOnFlag
-            goOnFlag = self.__delJumpToNextLine(linearList) or goOnFlag
-
-    def __plugLabels(self, linearList:List[StructureNode]) -> None:
-        """Branche les noeuds labels au noeud suivant
-        dans le but de fusionner les noeuds consécutifs
-
-        :param linearList: liste de noeuds de la version linéaire
-        :type linearList: list[StructureNode]
-        """
-        for index in range(len(linearList)-1):
-            item = linearList[index]
-            if isinstance(item, LabelNode):
-                nextItem = linearList[index+1]
-                if not item.plugTo(nextItem):
-                    raise CompilationError("Branchement noeud impossible ", {"lineNumber":item.lineNumber})
+        return "\n".join([str(item) for item in self._linearList])
 
     def __pushExpressionAsm(self, lineNumber:int, label:Optional[Label], expression:ExpressionNode) -> int:
         """Gère la compilation d'une expression arithmétique ou logique
@@ -164,9 +54,9 @@ class CompilationManager:
         :return: numéro du registre résultat ou -1 si inutile (comparaison)
         :rtype: int
         """
-        if not self.__engine.hasNEG():
+        if not self._engine.hasNEG():
             expression = expression.negToSubClone()
-        cem = CompileExpressionManager(self.__engine, self.__asm, lineNumber, label)
+        cem = CompileExpressionManager(self._engine, self._asm, lineNumber, label)
         expression.compile(cem)
         if expression.getType() == 'int':
             return cem.getResultRegister()
@@ -182,24 +72,26 @@ class CompilationManager:
         lineNumber = node.lineNumber
         if isinstance(node, AffectationNode):
             resultRegister = self.__pushExpressionAsm(lineNumber, node.label, node.expression)
-            self.__asm.pushStore(lineNumber, None, resultRegister, node.cible)
+            self._asm.pushStore(lineNumber, None, resultRegister, node.cible)
             return
         if isinstance(node, InputNode):
-            self.__asm.pushInput(lineNumber, node.label, node.cible)
+            self._asm.pushInput(lineNumber, node.label, node.cible)
             return
         if isinstance(node, PrintNode):
             resultRegister = self.__pushExpressionAsm(lineNumber, node.label, node.expression)
-            self.__asm.pushPrint(lineNumber, resultRegister)
+            self._asm.pushPrint(lineNumber, resultRegister)
             return
         if isinstance(node, JumpNode):
             labelCible = node.cible.label
             condition = node.getCondition()
             if not isinstance(condition,ExpressionNode):
-                self.__asm.pushJump(lineNumber, node.label, labelCible)
+                self._asm.pushJump(lineNumber, node.label, labelCible)
                 return
             comparaisonSymbol = condition.getComparaisonSymbol()
             self.__pushExpressionAsm(lineNumber, node.label, condition)
-            self.__asm.pushJump(lineNumber, None, labelCible, comparaisonSymbol)
+            self._asm.pushJump(lineNumber, None, labelCible, comparaisonSymbol)
+        if isinstance(node, SimpleNode) and node.snType == 'halt':
+            self._asm.pushHalt(node.label)
 
     def __compileASM(self) -> None:
         """Produit le code assembleur et conclut par HALT.
@@ -207,30 +99,17 @@ class CompilationManager:
 
         :return: pas de retour
         """
-        for node in self.__linearList:
+        for node in self._linearList.toList():
             self.__pushNodeAsm(node)
-        haltLabel = None
-        if len(self.__linearList)>0:
-            lastItem = self.__linearList[-1]
-            if isinstance(lastItem, LabelNode) and lastItem.isUnplugged():
-                haltLabel = lastItem.label
-        self.__asm.pushHalt(haltLabel)
 
-    def getLinearNodeList(self) -> List[StructureNode]:
-        """Accesseur
-
-        :return: programme sous forme linéaire
-        :rtype: list[StructureNode]
-        """
-        return [item for item in self.__linearList]
-
-    def getAsm(self) -> AssembleurContainer:
+    @property
+    def asm(self) -> AssembleurContainer:
         """Accesseur
 
         :return: objet assembleur contenant le résultat de la compilation
         :rtype: AssembleurContainer
         """
-        return self.__asm
+        return self._asm
 
 if __name__=="__main__":
     from expressionparser import ExpressionParser
@@ -251,10 +130,6 @@ if __name__=="__main__":
     ]
 
     cm = CompilationManager(engine, structuredList)
-    listCompiled = cm.getLinearNodeList()
-    for item in listCompiled:
-        print(item)
+    print(cm.asm)
     print()
-    print(cm.getAsm())
-    print()
-    print(cm.getAsm().getBinary())
+    print(cm.asm.getBinary())
