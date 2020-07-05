2ec772ec057be377ff35e4961e07bad9f76b4d12
gromax <gomaxx@gmail.com>
2020-07-05 17:13:22 +0200

    adaptation au nouveau format des gestionnaires de registres

--- modules/compileexpressionmanager.py
+++ modules/compileexpressionmanager.py
@@ -16,7 +16,7 @@ from modules.primitives.variable import Variable
 from modules.primitives.litteral import Litteral
 from modules.primitives.label import Label
 from modules.primitives.operators import Operator, Operators
-from modules.primitives.register import Register, RegisterBank, RegistersStack, TempMemoryStack
+from modules.primitives.register import Register, RegistersManager
 from modules.primitives.actionsfifo import ActionsFIFO
 
 from modules.engine.processorengine import ProcessorEngine
@@ -25,30 +25,20 @@ from modules.engine.processorengine import ProcessorEngine
 
 
 class CompileExpressionManager:
-    _registersBank : RegisterBank
-    _registersStack: RegistersStack
-    _memoryStack   : TempMemoryStack
+    _registers : RegistersManager
     _actions       : ActionsFIFO
 
-    def __init__(self, engine:ProcessorEngine, lineNumber:int):
-        """        Constructeur
+    def __init__(self, engine:ProcessorEngine, registers:RegistersManager):
+        """Constructeur
 
         :param engine: modèle de processeur utilisé
         :type engine: ProcessorEngine
-        :param lineNumber: numéro de la ligne d'origine
-        :type lineNumber: int
-        :type lineNumber: int
-
+        :param registers: gestionnaire de registres
+        :type registers: RegistersManager
         """
         self._engine = engine
-        self._registersNumber = self._engine.registersNumber()
-        assert self._registersNumber > 1
-        self._registersBank = RegisterBank(self._registersNumber)
-        self._registersStack = RegistersStack()
-        self._memoryStack = TempMemoryStack()
-
-        self._lineNumber = lineNumber
-        self._actions = ActionsFIFO()
+        self._registers = registers
+        self._registers.purgeStack()
 
 
     def compile(self, fifo:ActionsFIFO) -> ActionsFIFO:
@@ -56,12 +46,10 @@ class CompileExpressionManager:
 
         :param fifo: file de l'expression à compiler
         :type fifo: List[Union[Operator, Variable, Litteral]]
-        :param label: label du premier calcul
-        :type label: Label
         :return: file des actions à affectuer en tenant compte des mouvements de registres
         :rtype: ActionsFIFO
         """
-        self._actions.reset()
+        self._actions = ActionsFIFO()
         while not fifo.empty:
             self._compileNext(fifo)
         return self._actions
@@ -80,7 +68,7 @@ class CompileExpressionManager:
             self._pushValue(item)
             return
 
-        if item == Operators.SWAP.value:
+        if item == Operators.SWAP:
             self._swapStackRegister()
             return
 
@@ -120,9 +108,9 @@ class CompileExpressionManager:
 
         .. warning:: ne doit pas tomber sur une mémoire temporaire
         """
-        register: Optional[Register] = self._registersStack.pop()
+        register: Optional[Register] = self._registers.pop()
         if register is None:
-            raise CompilationError("Aucun registre à libérer.", {"lineNumber": self._lineNumber})
+            raise CompilationError("Aucun registre à libérer.")
         assert not register.isTemp
         return register
 
@@ -133,15 +121,15 @@ class CompileExpressionManager:
         :raises: CompilationError s'il n'y a pas d'opérande dans la pile
         .. note:: si le numéro est ``< 0``, provoque le dépilage d'un item de mémoire et retour du numéro de registre ayant accueilli le retour.
         """
-        register:Optional[Register] = self._registersStack.readTop()
+        register:Optional[Register] = self._registers.readTopStack()
         if register is None:
-            raise CompilationError("Pas assez d'opérande dans la pile.", {"lineNumber": self._lineNumber})
-        if register.isTemp:
-            self._registersStack.pop()
-            newRegister = self._copyMemoryToFreeRegister(register)
-            self._registersStack.push(newRegister)
-            return newRegister
-        return register
+            raise CompilationError("Pas assez d'opérande dans la pile.")
+        if not register.isTemp:
+            return register
+        self._registers.pop()
+        newRegister = self._copyMemoryToFreeRegister(register)
+        self._registers.push(newRegister)
+        return newRegister
 
     def _copyMemoryToFreeRegister(self, memoryRegister:Register) -> Register:
         """Libère la mémoire temporaire et la déplace dans un registre libre
@@ -152,10 +140,10 @@ class CompileExpressionManager:
         :rtype: Register
         :raises: CompilationError si aucun registre destination
         """
-        destinationRegister = self._registersBank.getFreeRegister()
+        destinationRegister = self._registers.getFreeRegister()
         if destinationRegister is None:
-            raise CompilationError("Pas de registre disponible", {"lineNumber": self._lineNumber})
-        self._actions.append(memoryRegister, destinationRegister, Operators.LOAD.value)
+            raise CompilationError("Pas de registre disponible")
+        self._actions.append(memoryRegister, destinationRegister, Operators.LOAD)
         return destinationRegister
 
     def _swapStackRegister(self):
@@ -163,7 +151,7 @@ class CompileExpressionManager:
 
         :raises: CompilationError s'il n'y a pas assez d'opérandes pour le swap
         """
-        self._registersStack.swap()
+        self._registers.swap()
 
     def _getAvailableRegister(self) -> Register:
         """Retourne un registre disponible.
@@ -172,10 +160,10 @@ class CompileExpressionManager:
         :rtype: int
         :raises: CompilationError si aucune registre n'est disponible
         """
-        register = self._registersBank.getFreeRegister()
+        register = self._registers.getFreeRegister()
         if register is None:
-            raise CompilationError("Pas de registre disponible", {"lineNumber": self._lineNumber})
-        self._registersStack.push(register)
+            raise CompilationError("Pas de registre disponible")
+        self._registers.push(register)
         return register
 
     def _UALoutputIsAvailable(self) -> bool:
@@ -183,7 +171,7 @@ class CompileExpressionManager:
         :return: vrai si la sortie de l'UAL est libre
         :rtype: bool
         """
-        return self._registersBank.isFree(0) or self._engine.ualOutputIsFree() and self._registersBank.hasAvailables()
+        return self._registers.isFree(0) or self._engine.ualOutputIsFree() and self._registers.hasAvailables()
 
     def _moveBottomRegisterToMemory(self):
         """Libère le registre en bas de la pile des registres utilisés.
@@ -191,12 +179,12 @@ class CompileExpressionManager:
         Place -1 dans la pile des registres utilisés pour indiquer le placement en mémoire temporaire.
         :raises: CompilationError si aucun registre à déplacer
         """
-        indexRegisterToCopyInTemp = self._registersStack.getLastRegisterIndex()
+        indexRegisterToCopyInTemp = self._registers.getLastRegisterIndexInStack()
         if indexRegisterToCopyInTemp == -1:
-            raise CompilationError("Pas de registre disponible", {"lineNumber": self._lineNumber})
-        registerToCopyInTemp = self._registersStack.extract(indexRegisterToCopyInTemp)
+            raise CompilationError("Pas de registre disponible")
+        registerToCopyInTemp = self._registers.extractFromStack(indexRegisterToCopyInTemp)
         memoryRegister = self._copyRegisterToMemory(registerToCopyInTemp)
-        self._registersStack.insert(indexRegisterToCopyInTemp, memoryRegister)
+        self._registers.insertInStack(indexRegisterToCopyInTemp, memoryRegister)
 
     def _copyRegisterToMemory(self, sourceRegister:Register) -> Register:
         """Ajoute au code assembleur l'opération consistant à déplacer un registre vers la mémoire.
@@ -207,28 +195,28 @@ class CompileExpressionManager:
         :return: Mémoire cible
         :rtype: Registrer
         """
-        memoryRegister = self._memoryStack.getFreeRegister()
-        self._actions.append(sourceRegister, memoryRegister, Operators.STORE.value)
+        memoryRegister = self._registers.getFreeTempRegister()
+        self._actions.append(sourceRegister, memoryRegister, Operators.STORE)
         return memoryRegister
 
     def _freeZeroRegister(self):
         """Libère spécifiquement le registre 0, même s'il n'est pas au sommet de la pile.
         Aucune opération s'il est libre.
         """
-        r0 = self._registersBank.getZeroRegister()
-        index0 = self._registersStack.index(r0)
+        r0 = self._registers.getZeroRegister()
+        index0 = self._registers.indexInStack(r0)
         if index0 < 0:
             return
 
-        freeRegister = self._registersBank.getFreeRegister()
+        freeRegister = self._registers.getFreeRegister()
         if freeRegister is None:
-            self._registersStack.extract(index0)
+            self._registers.extractFromStack(index0)
             memory = self._copyRegisterToMemory(r0)
-            self._registersStack.insert(index0, memory)
+            self._registers.insertInStack(index0, memory)
             return
-        self._registersStack.extract(index0)
-        self._registersStack.insert(index0, freeRegister)
-        self._actions.append(r0, freeRegister, Operators.MOVE.value)
+        self._registers.extractFromStack(index0)
+        self._registers.insertInStack(index0, freeRegister)
+        self._actions.append(r0, freeRegister, Operators.MOVE)
 
     def _pushBinaryOperator(self, operator:Operator):
         """Ajoute une opération binaire.
@@ -302,9 +290,9 @@ class CompileExpressionManager:
         """
         registreDestination = self._getAvailableRegister()
         if isinstance(value, Litteral):
-            self._actions.append(value, registreDestination, Operators.MOVE.value)
+            self._actions.append(value, registreDestination, Operators.MOVE)
         else:
-            self._actions.append(value, registreDestination, Operators.LOAD.value)
+            self._actions.append(value, registreDestination, Operators.LOAD)
 
 
     def _getNeededRegisterSpace(self, needUAL:bool):
@@ -315,14 +303,8 @@ class CompileExpressionManager:
         :param needUAL: le noeud nécessitera-t-il l'utilisation de l'UAL ?
         :type needUAL: bool
         """
-        if not self._registersBank.hasAvailables():
+        if not self._registers.hasAvailables():
             self._moveBottomRegisterToMemory()
         if needUAL and not self._UALoutputIsAvailable():
             self._freeZeroRegister()
 
-    def getResultRegister(self) -> int:
-        """
-        :return: registre en haut de la pile
-        :rtype: int
-        """
-        return self._getTopStackRegister()
