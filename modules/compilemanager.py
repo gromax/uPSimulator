0cd10b385f766576d4b0953ca1b7e1071e921b0b
gromax <gomaxx@gmail.com>
2020-07-06 00:07:59 +0200

    asm enlevé, structure simplifiée

--- modules/compilemanager.py
+++ modules/compilemanager.py
@@ -15,22 +15,27 @@
     expressions arithmétiques.
 """
 from typing import List, Dict, Optional, Tuple
+from typing_extensions import TypedDict
 
-from primitives.label import Label
+from modules.errors import CompilationError
+from modules.primitives.label import Label
+from modules.structuresnodes import StructureNode, StructureNodeList, JumpNode, SimpleNode, TransfertNode
+from modules.compileexpressionmanager import CompileExpressionManager
+from modules.engine.processorengine import ProcessorEngine
+from modules.expressionnodes.arithmetic import ArithmeticExpressionNode
+from modules.expressionnodes.comparaison import ComparaisonExpressionNode
+from modules.primitives.actionsfifo import ActionsFIFO
+from modules.primitives.register import RegistersManager
+from modules.primitives.operators import Operators
 
-from structuresnodes import StructureNode, StructureNodeList, JumpNode, InputNode, SimpleNode, AffectationNode, PrintNode
-
-from assembleurcontainer import AssembleurContainer
-from compileexpressionmanager import CompileExpressionManager
-from processorengine import ProcessorEngine
-from arithmeticexpressionnodes import ArithmeticExpressionNode
-from comparaisonexpressionnodes import ComparaisonExpressionNode
-
+#from assembleurcontainer import AssembleurContainer
+LineAction = TypedDict('LineAction', {'lineNumber': int, 'label':Optional[Label], 'actions': ActionsFIFO})
 
 class CompilationManager:
     _engine:ProcessorEngine
     _linearList:StructureNodeList
-    _asm:AssembleurContainer
+    _actionsList: List[LineAction]
+    _registers: RegistersManager
     def __init__(self, engine:ProcessorEngine, listOfStructureNodes:List[StructureNode]):
         """Constructeur
 
@@ -44,13 +49,13 @@ class CompilationManager:
         Fait immédiatement la compilation et crée un objet assembleur pour stocker le résultat.
         la liste fournie n'est pas stockée, on en produit une forme linéaire (while et if transformé) aussitôt.
         """
+        self._actionsList = []
         self._engine = engine
-        self._asm = AssembleurContainer(self._engine)
+        self._registers = RegistersManager(self._engine.registersNumber())
         self._linearList = StructureNodeList(listOfStructureNodes)
         comparaisonSymbolsAvailables = self._engine.getComparaisonSymbolsAvailables()
-        print(self._linearList)
         self._linearList.linearize(comparaisonSymbolsAvailables)
-        self._compileASM()
+        self._actionsList = [self._compileNode(node) for node in self._linearList]
 
     def __str__(self) -> str:
         """Transtypage -> str
@@ -60,114 +65,50 @@ class CompilationManager:
         """
         return "\n".join([str(item) for item in self._linearList])
 
-    def _pushArithmeticAsm(self, lineNumber:int, label:Optional[Label], expression:ArithmeticExpressionNode) -> int:
-        """Gère la compilation d'une expression arithmétique
-
-        :param lineNumber: numéro de ligne d'origine de l'expression
-        :type lineNumber: int
-        :param label: label du début de l'expression
-        :type label: Optional[Label]
-        :param expression: expression à compiler
-        :type expression: ArithmeticExpressionNode
-        :return: numéro du registre résultat ou -1 si inutile (comparaison)
-        :rtype: int
-        """
-        fifo = expression.getFIFO(self._engine.litteralMaxSize)
-        cem = CompileExpressionManager(self._engine, lineNumber)
-        cem.compile(fifo)
-        return cem.getResultRegister()
-
-    def _pushComparaisonAsm(self, lineNumber:int, label:Optional[Label], condition:ComparaisonExpressionNode) -> None:
-        """Gère la compilation d'une expression de comparaison
-
-        :param lineNumber: numéro de ligne d'origine de l'expression
-        :type lineNumber: int
-        :param label: label du début de l'expression
-        :type label: Optional[Label]
-        :param condition: comparaison à compiler
-        :type condition: ComparaisonExpressionNode
-        """
-        fifo = condition.getFIFO(self._engine.litteralMaxSize)
-        cem = CompileExpressionManager(self._engine, lineNumber)
-        cem.compile(fifo)
-
-    def _pushNodeAsm(self, node:StructureNode) -> None:
+    def _compileNode(self, node:StructureNode) -> LineAction:
         """Exécute la compilation pour un noeud. Le résultat est ajouté à l'objet assembleur.
 
         :param node: noeud à compiler
         :type node: StructureNode
-        :return: pas de retour.
+        :return: le maillon numéro de ligne, label, action fifo
+        :rtype: LineAction
         """
-        lineNumber = node.lineNumber
-        if isinstance(node, AffectationNode):
-            resultRegister = self._pushArithmeticAsm(lineNumber, node.label, node.expression)
-            self._asm.pushStore(lineNumber, None, resultRegister, node.cible)
-            return
-        if isinstance(node, InputNode):
-            self._asm.pushInput(lineNumber, node.label, node.cible)
-            return
-        if isinstance(node, PrintNode):
-            resultRegister = self._pushArithmeticAsm(lineNumber, node.label, node.expression)
-            self._asm.pushPrint(lineNumber, resultRegister)
-            return
+        
+        if isinstance(node, TransfertNode):
+            expression = node.expression
+            if not expression is None:
+                fifo = expression.getFIFO(self._engine.litteralMaxSize)
+                cem = CompileExpressionManager(self._engine, self._registers)
+                actionsItem = cem.compile(fifo)
+                resultRegister = self._registers.pop()
+                cible = node.cible
+                if cible is None:
+                    actionsItem.append(resultRegister, Operators.PRINT)
+                else:
+                    actionsItem.append(resultRegister, node.cible, Operators.STORE)
+            else:
+                cible = node.cible
+                assert not cible is None
+                actionsItem = ActionsFIFO()
+                actionsItem.append(node.cible, Operators.INPUT)
+            return {"lineNumber":node.lineNumber, "label":node.label, "actions":actionsItem}
+
         if isinstance(node, JumpNode):
             labelCible = node.cible.assignLabel()
             condition = node.getCondition()
-            if not isinstance(condition, ComparaisonExpressionNode):
-                self._asm.pushJump(lineNumber, node.label, labelCible)
-                return
-            comparaisonSymbol = condition.comparaisonSymbol
-            self._pushComparaisonAsm(lineNumber, node.label, condition)
-            self._asm.pushJump(lineNumber, None, labelCible, comparaisonSymbol)
-        if isinstance(node, SimpleNode) and node.snType == 'halt':
-            self._asm.pushHalt(node.label)
+            if condition is None:
+                actionsItem = ActionsFIFO()
+            else:
+                fifo = condition.getFIFO(self._engine.litteralMaxSize)
+                cem = CompileExpressionManager(self._engine, self._registers)
+                actionsItem = cem.compile(fifo)
+            actionsItem.append(labelCible, Operators.GOTO)
+            return {"lineNumber":node.lineNumber, "label":node.label, "actions":actionsItem}
 
-    def _compileASM(self) -> None:
-        """Produit le code assembleur et conclut par HALT.
-        Le résultat est stocké directement dans l'objet assembleur créé à l'initialisation.
+        if isinstance(node, SimpleNode) and not node.operator is None:
+            actionsItem = ActionsFIFO()
+            actionsItem.append(node.operator)
+            return {"lineNumber":node.lineNumber, "label":node.label, "actions":actionsItem}
 
-        :return: pas de retour
-        """
-        for node in self._linearList.toList():
-            self._pushNodeAsm(node)
+        raise CompilationError("Noeud non reconnu", {"lineNumber": node.lineNumber})
 
-    @property
-    def asm(self) -> AssembleurContainer:
-        """Accesseur
-
-        :return: objet assembleur contenant le résultat de la compilation
-        :rtype: AssembleurContainer
-        """
-        return self._asm
-
-if __name__=="__main__":
-    from expressionparser import ExpressionParser as EP
-    from processor16bits import Processor16Bits
-    from primitives.variable import Variable
-    from structuresnodes import WhileNode
-    from codeparser import CodeParser
-
-    engine = Processor16Bits()
-
-    varX = Variable('x')
-    varY = Variable('y')
-
-    affectationX = AffectationNode(4, varX, EP.buildExpression('-3*x+1')) # mypy: ignore
-    affectationY = AffectationNode(5, varY, EP.buildExpression('y+x')) # mypy: ignore
-    structuredList = [
-        AffectationNode(1, varX, EP.buildExpression('0')),
-        AffectationNode(2, varY, EP.buildExpression('0')),
-        WhileNode(3, EP.buildExpression('x < 10 or y < 100'), [affectationX, affectationY]),
-        PrintNode(6, EP.buildExpression('y'))
-    ]
-
-    cm = CompilationManager(engine, structuredList)
-    print(cm.asm)
-    print()
-    print(cm.asm.getBinary())
-
-    print()
-    print()
-    code = CodeParser.parse(filename = "example2.code")
-    cm = CompilationManager(engine, code)
-    print(cm.asm)
