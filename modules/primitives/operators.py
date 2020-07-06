c174bf9ade6319b74e66182b788a40f6736f01c4
gromax <gomaxx@gmail.com>
2020-07-06 22:36:53 +0200

    utilisation de constantes pour les différents types, ajout d'un type command

--- modules/primitives/operators.py
+++ modules/primitives/operators.py
@@ -6,26 +6,33 @@
 from typing import List
 
 class Operator:
+  ARITHMETIC  = 1
+  LOGIC       = 2
+  COMPARAISON = 3
+  COMMAND     = 4
+  OTHER       = 0
   _arity        :int
   _isCommutatif :bool
   _regex        :str
   _symbol       :str
-  _isArithmetic :bool
-  _isComparaison:bool
-  _isLogic      :bool
+  _type         :int
   _priority     :int
 
   @property
   def isArithmetic(self) -> bool:
-    return self._isArithmetic
+    return self._type == Operator.ARITHMETIC
 
   @property
   def isComparaison(self) -> bool:
-    return self._isComparaison
+    return self._type == Operator.COMPARAISON
 
   @property
   def isLogic(self) -> bool:
-    return self._isLogic
+    return self._type == Operator.LOGIC
+
+  @property
+  def isCommand(self) -> bool:
+    return self._type == Operator.COMMAND
 
   @property
   def isCommutatif(self) -> bool:
@@ -47,49 +54,47 @@ class Operator:
   def priority(self) -> int:
     return self._priority
 
-  def __init__(self, arity:int, isCommutatif:bool, regex:str, symbol:str, isArithmetic:bool, isComparaison:bool, isLogic:bool, priority:int):
+  def __init__(self, arity:int, isCommutatif:bool, regex:str, symbol:str, optype:int, priority:int):
     self._arity         = arity
     self._isCommutatif  = isCommutatif
     self._regex         = regex
     self._symbol        = symbol
-    self._isArithmetic  = isArithmetic
-    self._isComparaison = isComparaison
-    self._isLogic       = isLogic
+    self._type          = optype
     self._priority      = priority
 
   def __str__(self) -> str:
     return self._symbol
 
 class Operators:
-  ADD     : Operator = Operator(2, True , r"\+" , "+"  , True , False, False, 5)
-  MINUS   : Operator = Operator(2, False, r"\-" , "-"  , True , False, False, 5)
-  MULT    : Operator = Operator(2, True , r"\*" , "*"  , True , False, False, 7)
-  DIV     : Operator = Operator(2, False, r"\/" , "/"  , True , False, False, 7)
-  MOD     : Operator = Operator(2, False, "%"  , "%"  , True , False, False, 8)
-  NEG     : Operator = Operator(1, False, ""   , "-"  , True , False, False, 6)
-  OR      : Operator = Operator(2, True , r"\|" , "|"  , True , False, False, 6)
-  AND     : Operator = Operator(2, True , "&"  , "&"  , True , False, False, 7)
-  XOR     : Operator = Operator(2, True , r"\^" , "^"  , True , False, False, 7)
-  INVERSE : Operator = Operator(1, False, r"\~" , "~"  , True , False, False, 6)
-  SUP     : Operator = Operator(2, False, ">"  , ">"  , False, True , False, 4)
-  SUPOREQ : Operator = Operator(2, False, ">=" , ">=" , False, True , False, 4)
-  INF     : Operator = Operator(2, False, "<"  , "<"  , False, True , False, 4)
-  INFOREQ : Operator = Operator(2, False, "<=" , "<=" , False, True , False, 4)
-  EQ      : Operator = Operator(2, True , "=="  , "==", False, True , False, 4)
-  NOTEQ   : Operator = Operator(2, True , "!="  , "!=", False, True , False, 4)
-  LOGICOR : Operator = Operator(2, True , "or" , "or" , False, False, True , 1)
-  LOGICAND: Operator = Operator(2, True , "and", "and", False, False, True , 3)
-  LOGICNOT: Operator = Operator(1, False, "not", "not", False, False, True , 2)
-  SWAP    : Operator = Operator(2, False, ""   ,"swap", False, False, False, 0)
-  MOVE    : Operator = Operator(1, False, ""   ,"move", False, False, False, 0)
-  HALT    : Operator = Operator(0, False, ""   ,"halt", False, False, False, 0)
-  NOP     : Operator = Operator(0, False, ""   ,"nop", False, False, False, 0)
-  GOTO    : Operator = Operator(1, False, ""   ,"goto", False, False, False, 0)
-  LOAD    : Operator = Operator(1, False, ""   ,"load", False, False, False, 0)
-  STORE   : Operator = Operator(1, False, ""   ,"store", False, False, False, 0)
-  CMP     : Operator = Operator(2, False, ""   ,"cmp" , False, False, False, 0)
-  PRINT   : Operator = Operator(1, False, ""   ,"print", False, False, False, 0)
-  INPUT   : Operator = Operator(1, False, ""   ,"input", False, False, False, 0)
+  ADD     : Operator = Operator(2, True , r"\+" , "+"  , Operator.ARITHMETIC, 5)
+  MINUS   : Operator = Operator(2, False, r"\-" , "-"  , Operator.ARITHMETIC, 5)
+  MULT    : Operator = Operator(2, True , r"\*" , "*"  , Operator.ARITHMETIC, 7)
+  DIV     : Operator = Operator(2, False, r"\/" , "/"  , Operator.ARITHMETIC, 7)
+  MOD     : Operator = Operator(2, False, "%"  , "%"  , Operator.ARITHMETIC, 8)
+  NEG     : Operator = Operator(1, False, ""   , "-"  , Operator.ARITHMETIC, 6)
+  OR      : Operator = Operator(2, True , r"\|" , "|"  , Operator.ARITHMETIC, 6)
+  AND     : Operator = Operator(2, True , "&"  , "&"  , Operator.ARITHMETIC, 7)
+  XOR     : Operator = Operator(2, True , r"\^" , "^"  , Operator.ARITHMETIC, 7)
+  INVERSE : Operator = Operator(1, False, r"\~" , "~"  , Operator.ARITHMETIC, 6)
+  SUP     : Operator = Operator(2, False, ">"  , ">"  , Operator.COMPARAISON, 4)
+  SUPOREQ : Operator = Operator(2, False, ">=" , ">=" , Operator.COMPARAISON, 4)
+  INF     : Operator = Operator(2, False, "<"  , "<"  , Operator.COMPARAISON, 4)
+  INFOREQ : Operator = Operator(2, False, "<=" , "<=" , Operator.COMPARAISON, 4)
+  EQ      : Operator = Operator(2, True , "=="  , "==", Operator.COMPARAISON, 4)
+  NOTEQ   : Operator = Operator(2, True , "!="  , "!=", Operator.COMPARAISON, 4)
+  LOGICOR : Operator = Operator(2, True , "or" , "or" , Operator.LOGIC , 1)
+  LOGICAND: Operator = Operator(2, True , "and", "and", Operator.LOGIC , 3)
+  LOGICNOT: Operator = Operator(1, False, "not", "not", Operator.LOGIC , 2)
+  SWAP    : Operator = Operator(2, False, ""   ,"swap", Operator.OTHER, 0)
+  MOVE    : Operator = Operator(1, False, ""   ,"move", Operator.COMMAND, 0)
+  HALT    : Operator = Operator(0, False, ""   ,"halt", Operator.COMMAND, 0)
+  NOP     : Operator = Operator(0, False, ""   ,"nop", Operator.COMMAND, 0)
+  GOTO    : Operator = Operator(1, False, ""   ,"goto", Operator.COMMAND, 0)
+  LOAD    : Operator = Operator(1, False, ""   ,"load", Operator.COMMAND, 0)
+  STORE   : Operator = Operator(1, False, ""   ,"store", Operator.COMMAND, 0)
+  CMP     : Operator = Operator(2, False, ""   ,"cmp" , Operator.COMMAND, 0)
+  PRINT   : Operator = Operator(1, False, ""   ,"print", Operator.COMMAND, 0)
+  INPUT   : Operator = Operator(1, False, ""   ,"input", Operator.COMMAND, 0)
 
   @classmethod
   def list(cls) -> List[Operator]:
