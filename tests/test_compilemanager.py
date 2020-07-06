1208ab1e238fabc632b84a4d4e5c07ba187bf253
gromax <gomaxx@gmail.com>
2020-07-06 22:39:14 +0200

    création des tests

--- tests/test_compilemanager.py
+++ tests/test_compilemanager.py
@@ -6,40 +6,107 @@
 
 import unittest
 
+from modules.primitives.variable import Variable
+from modules.structuresnodes import TransfertNode, WhileNode
+from modules.parser.expression import ExpressionParser as EP
+from modules.engine.processor16bits import Processor16Bits
+from modules.compilemanager import CompilationManager as CM
+from modules.parser.code import CodeParser as CP
+
 class MyTest(unittest.TestCase):
     def test1(self):
-        self.assertEqual(True, False)
+        varX = Variable('x')
+        varY = Variable('y')
 
+        affectationX = TransfertNode(4, varX, EP.buildExpression('-3*x+1')) # mypy: ignore
+        affectationY = TransfertNode(5, varY, EP.buildExpression('y+x')) # mypy: ignore
+        structuredList = [
+            TransfertNode(1, varX, EP.buildExpression('0')),
+            TransfertNode(2, varY, EP.buildExpression('0')),
+            WhileNode(3, EP.buildExpression('x < 10 or y < 100'), [affectationX, affectationY]),
+            TransfertNode(6, None, EP.buildExpression('y'))
+        ]
 
-"""
-    from modules.parser.expression import ExpressionParser as EP
-    from processor16bits import Processor16Bits
-    from primitives.variable import Variable
-    from structuresnodes import WhileNode
-    from modules.parser.code import CodeParser
+        engine = Processor16Bits()
+        cm = CM(engine, structuredList)
+        actionsList = cm.compile()
+        strGlobal = "\n".join([str(item) for item in actionsList])
 
-    engine = Processor16Bits()
+        good = "\n".join([
+            "	#0 r7 move",
+            "	r7 @x store",
+            "	#0 r7 move",
+            "	r7 @y store",
+            "Lab2	@x r7 load",
+            "	#10 r6 move",
+            "	r7 r6 <",
+            "	Lab1 goto",
+            "	@y r7 load",
+            "	#100 r6 move",
+            "	r7 r6 <",
+            "	Lab1 goto",
+            "	Lab3 goto",
+            "Lab1	@x r7 load",
+            "	r7 #3 r7 *",
+            "	r7 r7 -",
+            "	r7 #1 r7 +",
+            "	r7 @x store",
+            "	@y r7 load",
+            "	@x r6 load",
+            "	r7 r6 r7 +",
+            "	r7 @y store",
+            "	Lab2 goto",
+            "Lab3	@y r7 load",
+            "	r7 print",
+            "	halt"
+        ])
+        self.assertEqual(strGlobal, good)
 
-    varX = Variable('x')
-    varY = Variable('y')
+    def test2(self):
+        code = CP.parse(filename = "example2.code")
+        engine = Processor16Bits()
+        cm = CM(engine, code)
+        actionsList = cm.compile()
+        strGlobal = "\n".join([str(item) for item in actionsList])
 
-    affectationX = AffectationNode(4, varX, EP.buildExpression('-3*x+1')) # mypy: ignore
-    affectationY = AffectationNode(5, varY, EP.buildExpression('y+x')) # mypy: ignore
-    structuredList = [
-        AffectationNode(1, varX, EP.buildExpression('0')),
-        AffectationNode(2, varY, EP.buildExpression('0')),
-        WhileNode(3, EP.buildExpression('x < 10 or y < 100'), [affectationX, affectationY]),
-        PrintNode(6, EP.buildExpression('y'))
-    ]
-
-    cm = CompilationManager(engine, structuredList)
-    print(cm.asm)
-    print()
-    print(cm.asm.getBinary())
-
-    print()
-    print()
-    code = CodeParser.parse(filename = "example2.code")
-    cm = CompilationManager(engine, code)
-    print(cm.asm)
-"""
+        good = "\n".join([
+            "	#0 r7 move",
+            "	r7 @s store",
+            "	#0 r7 move",
+            "	r7 @i store",
+            "	@m input",
+            "Lab2	@i r7 load",
+            "	@m r6 load",
+            "	r7 r6 <",
+            "	Lab1 goto",
+            "	Lab3 goto",
+            "Lab1	@i r7 load",
+            "	r7 #2 r7 %",
+            "	#0 r6 move",
+            "	r7 r6 ==",
+            "	Lab4 goto",
+            "	Lab5 goto",
+            "Lab4	@s r7 load",
+            "	@i r6 load",
+            "	r7 r6 r7 +",
+            "	r7 @s store",
+            "	@s r7 load",
+            "	#10 r6 move",
+            "	r7 r6 <",
+            "	Lab6 goto",
+            "	Lab5 goto",
+            "Lab6	@s r7 load",
+            "	r7 #2 r7 *",
+            "	r7 @y store",
+            "	@y r7 load",
+            "	r7 print",
+            "Lab5	@i r7 load",
+            "	r7 #3 r7 +",
+            "	r7 @i store",
+            "	Lab2 goto",
+            "Lab3	@s r7 load",
+            "	r7 print",
+            "	halt"
+        ])
+        print(strGlobal)
+        self.assertEqual(strGlobal, good)
