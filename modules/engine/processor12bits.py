2ef19637264f3b4f4a73323f59045b72545e6946
gromax <gomaxx@gmail.com>
2020-07-08 23:31:28 +0200

    divers
    - utilisation domain min max pour littéraux
    - séparation arithmétics commandes
    - gestion du move spécifique pour 16 bits

--- modules/engine/processor12bits.py
+++ modules/engine/processor12bits.py
@@ -3,43 +3,41 @@
    :synopsis: Exemple de modèle de processeur
 """
 
-from typing import Dict
+from typing import Dict, List, Tuple
 
 from modules.engine.processorengine import ProcessorEngine, Commands
 from modules.primitives.operators import Operators, Operator
-
+from modules.primitives.actionsfifo import ActionType
 
 class Processor12Bits(ProcessorEngine):
     _name                  :str = "Processeur 12 bits"
     _register_address_bits :int = 2
     _data_bits             :int = 12
     _freeUalOutput         :bool = False
-    _litteralMaxSize       :int = 0
+    _litteralDomain        :Tuple[int,int] = (0,0)
+
+    _litteralArithmeticsCommands :Dict[Operator,Commands] = {}
     _litteralsCommands     :Dict[Operator,Commands] = {}
     _commands              :Dict[Operator,Commands] = {
-            Operators.HALT    : { "opcode":"0000", "asm":"HALT" },
-            Operators.GOTO    : { "opcode":"0001", "asm":"JMP" },
-            Operators.EQ      : { "opcode":"0010", "asm":"BEQ" },
-            Operators.INF     : { "opcode":"0011", "asm":"BLT" },
-            Operators.CMP     : { "opcode":"11110101", "asm":"CMP" },
-            Operators.PRINT   : { "opcode":"0100", "asm":"PRINT" },
-            Operators.INPUT   : { "opcode":"0101", "asm":"INPUT" },
-            Operators.LOAD    : { "opcode":"100", "asm":"LOAD" },
-            Operators.MOVE    : { "opcode":"11110110", "asm":"MOVE" },
-            Operators.INVERSE : { "opcode":"11110111", "asm":"NOT" },
-            Operators.ADD     : { "opcode":"11111000", "asm":"ADD" },
-            Operators.MINUS   : { "opcode":"11111001", "asm":"SUB" },
-            Operators.MULT    : { "opcode":"11111010", "asm":"MULT" },
-            Operators.DIV     : { "opcode":"11111011", "asm":"DIV" },
-            Operators.MOD     : { "opcode":"11111100", "asm":"MOD" },
-            Operators.AND     : { "opcode":"11111101", "asm":"AND" },
-            Operators.OR      : { "opcode":"11111110", "asm":"OR" },
-            Operators.XOR     : { "opcode":"11111111", "asm":"XOR" },
-            Operators.STORE   : { "opcode":"101", "asm":"STORE" }
+        Operators.HALT    : { "opcode":"0000", "asm":"HALT" },
+        Operators.GOTO    : { "opcode":"0001", "asm":"JMP" },
+        Operators.EQ      : { "opcode":"0010", "asm":"BEQ" },
+        Operators.INF     : { "opcode":"0011", "asm":"BLT" },
+        Operators.CMP     : { "opcode":"11110101", "asm":"CMP" },
+        Operators.PRINT   : { "opcode":"0100", "asm":"PRINT" },
+        Operators.INPUT   : { "opcode":"0101", "asm":"INPUT" },
+        Operators.LOAD    : { "opcode":"100", "asm":"LOAD" },
+        Operators.MOVE    : { "opcode":"11110110", "asm":"MOVE" },
+        Operators.INVERSE : { "opcode":"11110111", "asm":"NOT" },
+        Operators.ADD     : { "opcode":"11111000", "asm":"ADD" },
+        Operators.MINUS   : { "opcode":"11111001", "asm":"SUB" },
+        Operators.MULT    : { "opcode":"11111010", "asm":"MULT" },
+        Operators.DIV     : { "opcode":"11111011", "asm":"DIV" },
+        Operators.MOD     : { "opcode":"11111100", "asm":"MOD" },
+        Operators.AND     : { "opcode":"11111101", "asm":"AND" },
+        Operators.OR      : { "opcode":"11111110", "asm":"OR" },
+        Operators.XOR     : { "opcode":"11111111", "asm":"XOR" },
+        Operators.STORE   : { "opcode":"101", "asm":"STORE" }
     }
 
 
-
-if __name__=="__main__":
-    import doctest
-    doctest.testmod()
--- modules/engine/processor16bits.py
+++ modules/engine/processor16bits.py
@@ -3,52 +3,82 @@
    :synopsis: Exemple de modèle de processeur
 """
 
-from typing import Dict
+from typing import Dict, List, Tuple
 
 from modules.engine.processorengine import ProcessorEngine, Commands
 from modules.primitives.operators import Operators, Operator
+from modules.primitives.actionsfifo import ActionType
+from modules.primitives.litteral import Litteral
 
 class Processor16Bits(ProcessorEngine):
     _name                  :str = "Processeur 16 bits"
     _register_address_bits :int = 3
     _data_bits             :int = 16
     _freeUalOutput         :bool = True
-    _litteralMaxSize       :int = 6
-    _litteralsCommands     :Dict[Operator,Commands] = {
-            Operators.NEG          : { "opcode":"010110", "asm":"NEG" },
-            Operators.MOVE         : { "opcode":"01001", "asm":"MOVE" },
-            Operators.ADD          : { "opcode":"1000", "asm":"ADD" },
-            Operators.MINUS        : { "opcode":"1001", "asm":"SUB" },
-            Operators.MULT         : { "opcode":"1010", "asm":"MULT" },
-            Operators.DIV          : { "opcode":"1011", "asm":"DIV" },
-            Operators.MOD          : { "opcode":"1100", "asm":"MOD" },
-            Operators.AND          : { "opcode":"1101", "asm":"AND" },
-            Operators.OR           : { "opcode":"1110", "asm":"OR" },
-            Operators.XOR          : { "opcode":"1111", "asm":"XOR" },
-            Operators.INVERSE      : { "opcode":"010111", "asm":"NOT" }
-    }
-    _commands              :Dict[Operator,Commands] = {
-            Operators.HALT         : { "opcode":"00000", "asm":"HALT" },
-            Operators.GOTO         : { "opcode":"00001", "asm":"JMP" },
-            Operators.NOTEQ        : { "opcode":"0001000", "asm":"BNE" },
-            Operators.EQ           : { "opcode":"0001001", "asm":"BEQ" },
-            Operators.INF          : { "opcode":"0001010", "asm":"BLT" },
-            Operators.SUP          : { "opcode":"0001011", "asm":"BGT" },
-            Operators.CMP          : { "opcode":"00011", "asm":"CMP" },
-            Operators.PRINT        : { "opcode":"00100", "asm":"PRINT" },
-            Operators.INPUT        : { "opcode":"00101", "asm":"INPUT" },
-            Operators.LOAD         : { "opcode":"0011", "asm":"LOAD" },
-            Operators.MOVE         : { "opcode":"01000", "asm":"MOVE" },
-            Operators.NEG          : { "opcode":"010100", "asm":"NEG"},
-            Operators.INVERSE      : { "opcode":"010101", "asm":"NOT"},
-            Operators.ADD          : { "opcode":"0110000", "asm":"ADD"},
-            Operators.MINUS        : { "opcode":"0110001", "asm":"SUB"},
-            Operators.MULT         : { "opcode":"0110010", "asm":"MULT"},
-            Operators.DIV          : { "opcode":"0110011", "asm":"DIV"},
-            Operators.MOD          : { "opcode":"0110100", "asm":"MOD"},
-            Operators.AND          : { "opcode":"0110101", "asm":"AND"},
-            Operators.OR           : { "opcode":"0110110", "asm":"OR"},
-            Operators.XOR          : { "opcode":"0110111", "asm":"XOR"},
-            Operators.STORE        : { "opcode":"0111", "asm":"STORE"}
+    _litteralDomain        :Tuple[int,int] = (0,63)
+    
+    _litteralArithmeticsCommands: Dict[Operator,Commands] = {
+        Operators.NEG      : { "opcode":"010110", "asm":"NEG" },
+        Operators.ADD      : { "opcode":"1000", "asm":"ADD" },
+        Operators.MINUS    : { "opcode":"1001", "asm":"SUB" },
+        Operators.MULT     : { "opcode":"1010", "asm":"MULT" },
+        Operators.DIV      : { "opcode":"1011", "asm":"DIV" },
+        Operators.MOD      : { "opcode":"1100", "asm":"MOD" },
+        Operators.AND      : { "opcode":"1101", "asm":"AND" },
+        Operators.OR       : { "opcode":"1110", "asm":"OR" },
+        Operators.XOR      : { "opcode":"1111", "asm":"XOR" },
+        Operators.INVERSE  : { "opcode":"010111", "asm":"NOT" }
     }
 
+    _arithmeticsCommands: Dict[Operator,Commands] = {
+        Operators.NEG      : { "opcode":"010100", "asm":"NEG"},
+        Operators.INVERSE  : { "opcode":"010101", "asm":"NOT"},
+        Operators.ADD      : { "opcode":"0110000", "asm":"ADD"},
+        Operators.MINUS    : { "opcode":"0110001", "asm":"SUB"},
+        Operators.MULT     : { "opcode":"0110010", "asm":"MULT"},
+        Operators.DIV      : { "opcode":"0110011", "asm":"DIV"},
+        Operators.MOD      : { "opcode":"0110100", "asm":"MOD"},
+        Operators.AND      : { "opcode":"0110101", "asm":"AND"},
+        Operators.OR       : { "opcode":"0110110", "asm":"OR"},
+        Operators.XOR      : { "opcode":"0110111", "asm":"XOR"}
+    }
+
+    _litteralsCommands     :Dict[Operator,Commands] = {
+         Operators.MOVE     : { "opcode":"01001", "asm":"MOVE" },
+    }
+
+    _commands              :Dict[Operator,Commands] = {
+        Operators.HALT         : { "opcode":"00000", "asm":"HALT" },
+        Operators.GOTO         : { "opcode":"00001", "asm":"JMP" },
+        Operators.NOTEQ        : { "opcode":"0001000", "asm":"BNE" },
+        Operators.EQ           : { "opcode":"0001001", "asm":"BEQ" },
+        Operators.INF          : { "opcode":"0001010", "asm":"BLT" },
+        Operators.SUP          : { "opcode":"0001011", "asm":"BGT" },
+        Operators.CMP          : { "opcode":"00011", "asm":"CMP" },
+        Operators.PRINT        : { "opcode":"00100", "asm":"PRINT" },
+        Operators.INPUT        : { "opcode":"00101", "asm":"INPUT" },
+        Operators.LOAD         : { "opcode":"0011", "asm":"LOAD" },
+        Operators.MOVE         : { "opcode":"01000", "asm":"MOVE" },
+        Operators.STORE        : { "opcode":"0111", "asm":"STORE"}
+    }
+
+    # hérité
+    def litteralOperatorAvailable(self, operator:Operator, litteral:Litteral) -> bool:
+        """Teste si la commande peut s'éxécuter dans une version acceptant un littéral, avec ce littéral en particulier. Il faut que la commande accepte les littéraux et que le codage de ce littéral soit possible dans l'espace laissé par cette commande.
+
+        :param operator: commande à utiliser
+        :type operator: Operator
+        :param litteral: littéral à utiliser
+        :type litteral: Litteral
+        :return: vrai si la commande est utilisable avec ce littéral
+        :rtype: bool
+        """
+
+        if operator in self._litteralArithmeticsCommands:
+            minVal, maxVal = self._litteralDomain
+            return litteral.isBetween(minVal, maxVal)
+        if operator == Operators.MOVE:
+            return litteral.isBetween(0, 255)
+        return False
+        
+        
