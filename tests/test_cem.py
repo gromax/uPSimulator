ce4f54c098304a7f3f5187db7cb01eea64a658be
gromax <gomaxx@gmail.com>
2020-07-08 23:33:16 +0200

    prise en compte de l'absence move pour littéral pour certains proc

--- tests/test_cem.py
+++ tests/test_cem.py
@@ -19,18 +19,18 @@ class LineTest(unittest.TestCase):
         cem = CompileExpressionManager(engine, regManager)
 
         parsed = ExpressionParser.buildExpression(strExpression)
-        fifo = parsed.getFIFO(engine.litteralMaxSize)
+        fifo = parsed.getFIFO(engine.litteralDomain)
         
         
         actions = cem.compile(fifo)
         
         good = "\n".join([
             "	@x r3 load",
-            "	#2 r2 move",
+            "	@#2 r2 load",
             "	r3 r2 r3 *",
-            "	#4 r2 move",
+            "	@#4 r2 load",
             "	r3 r2 r3 +",
-            "	#5 r2 move",
+            "	@#5 r2 load",
             "	r3 r2 r3 *",
             "	@x r2 load",
             "	@y r1 load",
@@ -41,45 +41,69 @@ class LineTest(unittest.TestCase):
         self.assertEqual(str(actions), good)
 
     def test2(self):
+        strExpression = "5*(2*x+4)-x*y"
+        engine = Processor16Bits()
+        regManager = RegistersManager(engine.registersNumber())
+        cem = CompileExpressionManager(engine, regManager)
+
+        parsed = ExpressionParser.buildExpression(strExpression)
+        fifo = parsed.getFIFO(engine.litteralDomain)
+       
+        actions = cem.compile(fifo)
+        
+        good = "\n".join([
+            "	@x r7 load",
+            "	@y r6 load",
+            "	r7 r6 r7 *",
+            "	@x r6 load",
+            "	r6 #2 r6 *",
+            "	r6 #4 r6 +",
+            "	r6 #5 r6 *",
+            "	r6 r7 r7 -"
+        ])
+        
+        self.assertEqual(str(actions), good)
+
+    def test3(self):
         strExpression = "(((2+4)*(4+1)) - ((2+4)*(4+1))) * (((2+4)*(4+1)) - ((2+4)*(4+1)))"
         engine = Processor12Bits()
         regManager = RegistersManager(engine.registersNumber())
 
         cem = CompileExpressionManager(engine, regManager)
         parsed = ExpressionParser.buildExpression(strExpression)
-        fifo = parsed.getFIFO(engine.litteralMaxSize)
+        fifo = parsed.getFIFO(engine.litteralDomain)
         actions = cem.compile(fifo)
         
         good = "\n".join([
-            "	#4 r3 move",
-            "	#2 r2 move",
+            "	@#4 r3 load",
+            "	@#2 r2 load",
             "	r3 r2 r3 +",
-            "	#4 r2 move",
-            "	#1 r1 move",
+            "	@#4 r2 load",
+            "	@#1 r1 load",
             "	r2 r1 r2 +",
             "	r3 r2 r3 *",
-            "	#4 r2 move",
-            "	#2 r1 move",
+            "	@#4 r2 load",
+            "	@#2 r1 load",
             "	r2 r1 r2 +",
-            "	#4 r1 move",
-            "	#1 r0 move",
+            "	@#4 r1 load",
+            "	@#1 r0 load",
             "	r1 r0 r1 +",
             "	r2 r1 r2 *",
             "	r3 r2 r3 -",
-            "	#4 r2 move",
-            "	#2 r1 move",
+            "	@#4 r2 load",
+            "	@#2 r1 load",
             "	r2 r1 r2 +",
-            "	#4 r1 move",
-            "	#1 r0 move",
+            "	@#4 r1 load",
+            "	@#1 r0 load",
             "	r1 r0 r1 +",
             "	r2 r1 r2 *",
-            "	#4 r1 move",
-            "	#2 r0 move",
+            "	@#4 r1 load",
+            "	@#2 r0 load",
             "	r1 r0 r1 +",
-            "	#4 r0 move",
+            "	@#4 r0 load",
             "	r3 _m0 store",
             "	r0 r3 move",
-            "	#1 r0 move",
+            "	@#1 r0 load",
             "	r3 r0 r3 +",
             "	r1 r3 r3 *",
             "	r2 r3 r3 -",
@@ -89,22 +113,21 @@ class LineTest(unittest.TestCase):
         
         self.assertEqual(str(actions), good)
 
-    def test3(self):
+    def test4(self):
         strExpression = "3*x > 4"
-        engine = Processor12Bits()
+        engine = Processor16Bits()
         regManager = RegistersManager(engine.registersNumber())
 
         cem = CompileExpressionManager(engine, regManager)
         parsed = ExpressionParser.buildExpression(strExpression)
-        fifo = parsed.getFIFO(engine.litteralMaxSize)
+        fifo = parsed.getFIFO(engine.litteralDomain)
         actions = cem.compile(fifo)
         
         good = "\n".join([
-            "	@x r3 load",
-            "	#3 r2 move",
-            "	r3 r2 r3 *",
-            "	#4 r2 move",
-            "	r3 r2 >"
+            "	@x r7 load",
+            "	r7 #3 r7 *",
+            "	#4 r6 move",
+            "	r7 r6 >"
         ])
         
         self.assertEqual(str(actions), good)
