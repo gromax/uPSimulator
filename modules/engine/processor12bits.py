900f2afbbc1b919ec707da84d6fd2f8204da4ae3
gromax <gomaxx@gmail.com>
2020-07-12 19:26:59 +0200

    MAJ des commandes avec asmgenerator

--- modules/engine/processor12bits.py
+++ modules/engine/processor12bits.py
@@ -5,9 +5,39 @@
 
 from typing import Dict, List, Tuple
 
-from modules.engine.processorengine import ProcessorEngine, Commands
+from modules.engine.processorengine import ProcessorEngine
 from modules.primitives.operators import Operators, Operator
 from modules.primitives.actionsfifo import ActionType
+from modules.primitives.litteral import Litteral
+from modules.primitives.variable import Variable
+from modules.primitives.register import Register
+from modules.primitives.label import Label
+from modules.engine.asmgenerator import AsmGenerator ,AsmGenerator_SINGLE, AsmGenerator_TRANSFERT, AsmGenerator_CONDITIONAL_GOTO
+
+'''
+HALT : 000000000000
+GOTO : 0001XXXXXXXX
+CMP  : 11110101XXXX
+BEQ  : 0010XXXXXXXX
+BLT  : 0011XXXXXXXX
+
+PRINT   : 0100XXXXXXXX
+INPUT   : 0101XXXXXXXX
+LOAD    : 100XXXXXXXXX
+MOVE    : 11110110XXXX
+INVERSE : 11110111XXXX
+ADD     : 11111000XXXX
+MINUS   : 11111001XXXX
+MULT    : 11111010XXXX
+DIV     : 11111011XXXX
+MOD     : 11111100XXXX
+AND     : 11111101XXXX
+OR      : 11111110XXXX
+XOR     : 11111111XXXX
+STORE   : 101XXXXXXXXX
+'''
+
+
 
 class Processor12Bits(ProcessorEngine):
     _name                  :str = "Processeur 12 bits"
@@ -16,28 +46,32 @@ class Processor12Bits(ProcessorEngine):
     _freeUalOutput         :bool = False
     _litteralDomain        :Tuple[int,int] = (0,0)
 
-    _litteralArithmeticsCommands :Dict[Operator,Commands] = {}
-    _litteralsCommands     :Dict[Operator,Commands] = {}
-    _commands              :Dict[Operator,Commands] = {
-        Operators.HALT    : { "opcode":"0000", "asm":"HALT" },
-        Operators.GOTO    : { "opcode":"0001", "asm":"JMP" },
-        Operators.EQ      : { "opcode":"0010", "asm":"BEQ" },
-        Operators.INF     : { "opcode":"0011", "asm":"BLT" },
-        Operators.CMP     : { "opcode":"11110101", "asm":"CMP" },
-        Operators.PRINT   : { "opcode":"0100", "asm":"PRINT" },
-        Operators.INPUT   : { "opcode":"0101", "asm":"INPUT" },
-        Operators.LOAD    : { "opcode":"100", "asm":"LOAD" },
-        Operators.MOVE    : { "opcode":"11110110", "asm":"MOVE" },
-        Operators.INVERSE : { "opcode":"11110111", "asm":"NOT" },
-        Operators.ADD     : { "opcode":"11111000", "asm":"ADD" },
-        Operators.MINUS   : { "opcode":"11111001", "asm":"SUB" },
-        Operators.MULT    : { "opcode":"11111010", "asm":"MULT" },
-        Operators.DIV     : { "opcode":"11111011", "asm":"DIV" },
-        Operators.MOD     : { "opcode":"11111100", "asm":"MOD" },
-        Operators.AND     : { "opcode":"11111101", "asm":"AND" },
-        Operators.OR      : { "opcode":"11111110", "asm":"OR" },
-        Operators.XOR     : { "opcode":"11111111", "asm":"XOR" },
-        Operators.STORE   : { "opcode":"101", "asm":"STORE" }
-    }
+    _comparaisonOperators = [
+        Operators.EQ,
+        Operators.INF
+    ]
+
+    _asmGenerators:List[AsmGenerator] = [
+        AsmGenerator_TRANSFERT(Operators.NEG,     "NEG",   "11110110#2#.0.2", operands = [Register, Register]),
+        AsmGenerator_TRANSFERT(Operators.INVERSE, "NOT",   "11110111#2#.0.2", operands = [Register, Register]),
+        AsmGenerator_TRANSFERT(Operators.ADD,     "ADD",   "11111000.0.2.2", operands = [Register, Register, Register]),
+        AsmGenerator_TRANSFERT(Operators.MINUS,   "SUB",   "11111001.0.2.2", operands = [Register, Register, Register]),
+        AsmGenerator_TRANSFERT(Operators.MULT,    "MULT",  "11111010.0.2.2", operands = [Register, Register, Register]),
+        AsmGenerator_TRANSFERT(Operators.DIV,     "DIV",   "11111011.0.2.2", operands = [Register, Register, Register]),
+        AsmGenerator_TRANSFERT(Operators.MOD,     "MOD",   "11111100.0.2.2", operands = [Register, Register, Register]),
+        AsmGenerator_TRANSFERT(Operators.AND,     "AND",   "11111101.0.2.2", operands = [Register, Register, Register]),
+        AsmGenerator_TRANSFERT(Operators.OR,      "OR",    "11111110.0.2.2", operands = [Register, Register, Register]),
+        AsmGenerator_TRANSFERT(Operators.XOR,     "XOR",   "11111111.0.2.2", operands = [Register, Register, Register]),
+        AsmGenerator_TRANSFERT(Operators.GOTO,    "MOVE",  "11110110.2.2", operands = [Register, Register]),
+        AsmGenerator_TRANSFERT(Operators.PRINT,   "PRINT", "0100#6#.2", operands = [Register]),
+        AsmGenerator_TRANSFERT(Operators.INPUT,   "INPUT", "0101.8", operands = [Variable]),
+        AsmGenerator_TRANSFERT(Operators.LOAD,    "LOAD",  "100.2.7", operands = [Variable, Register]),
+        AsmGenerator_TRANSFERT(Operators.STORE,   "STORE", "101.7.2", operands = [Register, Variable]),
+        AsmGenerator_SINGLE(Operators.HALT,       "HALT",  "#12"),
+        AsmGenerator_TRANSFERT(Operators.GOTO,    "JMP",   "0001.8", operands = [Label]),
+        AsmGenerator_CONDITIONAL_GOTO(Operators.GOTO, "CMP;BEQ", "11110101.2.2.0010.8", comparator = Operators.EQ),
+        AsmGenerator_CONDITIONAL_GOTO(Operators.GOTO, "CMP;BLT", "11110101.2.2.0011.8", comparator = Operators.INF)
+    ]
+
 
 
--- modules/engine/processor16bits.py
+++ modules/engine/processor16bits.py
@@ -5,10 +5,54 @@
 
 from typing import Dict, List, Tuple
 
-from modules.engine.processorengine import ProcessorEngine, Commands
+from modules.engine.processorengine import ProcessorEngine
 from modules.primitives.operators import Operators, Operator
 from modules.primitives.actionsfifo import ActionType
 from modules.primitives.litteral import Litteral
+from modules.primitives.variable import Variable
+from modules.primitives.register import Register
+from modules.primitives.label import Label
+from modules.engine.asmgenerator import AsmGenerator ,AsmGenerator_SINGLE, AsmGenerator_TRANSFERT, AsmGenerator_CONDITIONAL_GOTO
+
+
+'''
+littéraux
+NEG     : 010110XXXXXXXXXX
+INVERSE : 010111XXXXXXXXXX
+ADD     : 1000XXXXXXXXXXXX
+MINUS   : 1001XXXXXXXXXXXX
+MULT    : 1010XXXXXXXXXXXX
+DIV     : 1011XXXXXXXXXXXX
+MOD     : 1100XXXXXXXXXXXX
+AND     : 1101XXXXXXXXXXXX
+OR      : 1110XXXXXXXXXXXX
+XOR     : 1111XXXXXXXXXXXX
+MOVE    : 01001XXXXXXXXXXX
+autres
+HALT    : 0000XXXXXXXXXXXX
+NEG     : 010100XXXXXXXXXX
+INVERSE : 010101XXXXXXXXXX
+ADD     : 0110000XXXXXXXXX
+MINUS   : 0110001XXXXXXXXX
+MULT    : 0110010XXXXXXXXX
+DIV     : 0110011XXXXXXXXX
+MOD     : 0110100XXXXXXXXX
+AND     : 0110101XXXXXXXXX
+OR      : 0110110XXXXXXXXX
+XOR     : 0110111XXXXXXXXX
+HALT    : 00000XXXXXXXXXXX
+GOTO    : 00001XXXXXXXXXXX
+NOTEQ   : 0001000XXXXXXXXX
+EQ      : 0001001XXXXXXXXX
+INF     : 0001010XXXXXXXXX
+SUP     : 0001011XXXXXXXXX
+CMP     : 00011XXXXXXXXXXX
+PRINT   : 00100XXXXXXXXXXX
+INPUT   : 00101XXXXXXXXXXX
+LOAD    : 0011XXXXXXXXXXXX
+MOVE    : 01000XXXXXXXXXXX
+STORE   : 0111XXXXXXXXXXXX
+'''
 
 class Processor16Bits(ProcessorEngine):
     _name                  :str = "Processeur 16 bits"
@@ -17,50 +61,49 @@ class Processor16Bits(ProcessorEngine):
     _freeUalOutput         :bool = True
     _litteralDomain        :Tuple[int,int] = (0,63)
     
-    _litteralArithmeticsCommands: Dict[Operator,Commands] = {
-        Operators.NEG      : { "opcode":"010110", "asm":"NEG" },
-        Operators.ADD      : { "opcode":"1000", "asm":"ADD" },
-        Operators.MINUS    : { "opcode":"1001", "asm":"SUB" },
-        Operators.MULT     : { "opcode":"1010", "asm":"MULT" },
-        Operators.DIV      : { "opcode":"1011", "asm":"DIV" },
-        Operators.MOD      : { "opcode":"1100", "asm":"MOD" },
-        Operators.AND      : { "opcode":"1101", "asm":"AND" },
-        Operators.OR       : { "opcode":"1110", "asm":"OR" },
-        Operators.XOR      : { "opcode":"1111", "asm":"XOR" },
-        Operators.INVERSE  : { "opcode":"010111", "asm":"NOT" }
-    }
+    _comparaisonOperators = [
+        Operators.NOTEQ,
+        Operators.EQ,
+        Operators.INF,
+        Operators.SUP
+    ]
 
-    _arithmeticsCommands: Dict[Operator,Commands] = {
-        Operators.NEG      : { "opcode":"010100", "asm":"NEG"},
-        Operators.INVERSE  : { "opcode":"010101", "asm":"NOT"},
-        Operators.ADD      : { "opcode":"0110000", "asm":"ADD"},
-        Operators.MINUS    : { "opcode":"0110001", "asm":"SUB"},
-        Operators.MULT     : { "opcode":"0110010", "asm":"MULT"},
-        Operators.DIV      : { "opcode":"0110011", "asm":"DIV"},
-        Operators.MOD      : { "opcode":"0110100", "asm":"MOD"},
-        Operators.AND      : { "opcode":"0110101", "asm":"AND"},
-        Operators.OR       : { "opcode":"0110110", "asm":"OR"},
-        Operators.XOR      : { "opcode":"0110111", "asm":"XOR"}
-    }
+    _asmGenerators: List[AsmGenerator] = [
+        AsmGenerator_TRANSFERT(Operators.NEG,     "NEG",   "010110.3.7",     operands = [Litteral, Register]),
+        AsmGenerator_TRANSFERT(Operators.INVERSE, "NOT",   "010111.3.7",     operands = [Litteral, Register]),
+        AsmGenerator_TRANSFERT(Operators.ADD,     "ADD",   "1000.3.3.6",     operands = [Register, Litteral, Register]),
+        AsmGenerator_TRANSFERT(Operators.MINUS,   "SUB",   "1001.3.3.6",     operands = [Register, Litteral, Register]),
+        AsmGenerator_TRANSFERT(Operators.MULT,    "MULT",  "1010.3.3.6",     operands = [Register, Litteral, Register]),
+        AsmGenerator_TRANSFERT(Operators.DIV,     "DIV",   "1011.3.3.6",     operands = [Register, Litteral, Register]),
+        AsmGenerator_TRANSFERT(Operators.MOD,     "MOD",   "1100.3.3.6",     operands = [Register, Litteral, Register]),
+        AsmGenerator_TRANSFERT(Operators.AND,     "AND",   "1101.3.3.6",     operands = [Register, Litteral, Register]),
+        AsmGenerator_TRANSFERT(Operators.OR,      "OR",    "1110.3.3.6",     operands = [Register, Litteral, Register]),
+        AsmGenerator_TRANSFERT(Operators.XOR,     "XOR",   "1111.3.3.6",     operands = [Register, Litteral, Register]),
+        AsmGenerator_TRANSFERT(Operators.MOVE,    "MOVE",  "01001.3.8",      operands = [Litteral, Register]),
+        AsmGenerator_TRANSFERT(Operators.NEG,     "NEG",   "010100#4#.3.3", operands = [Register, Register]),
+        AsmGenerator_TRANSFERT(Operators.INVERSE, "NOT",   "010101#4#.3.3", operands = [Register, Register]),
+        AsmGenerator_TRANSFERT(Operators.ADD,     "ADD",   "0110000.3.3.3",  operands = [Register, Register, Register]),
+        AsmGenerator_TRANSFERT(Operators.MINUS,   "SUB",   "0110001.3.3.3",  operands = [Register, Register, Register]),
+        AsmGenerator_TRANSFERT(Operators.MULT,    "MULT",  "0110010.3.3.3",  operands = [Register, Register, Register]),
+        AsmGenerator_TRANSFERT(Operators.DIV,     "DIV",   "0110011.3.3.3",  operands = [Register, Register, Register]),
+        AsmGenerator_TRANSFERT(Operators.MOD,     "MOD",   "0110100.3.3.3",  operands = [Register, Register, Register]),
+        AsmGenerator_TRANSFERT(Operators.AND,     "AND",   "0110101.3.3.3",  operands = [Register, Register, Register]),
+        AsmGenerator_TRANSFERT(Operators.OR,      "OR",    "0110110.3.3.3",  operands = [Register, Register, Register]),
+        AsmGenerator_TRANSFERT(Operators.XOR,     "XOR",   "0110111.3.3.3",  operands = [Register, Register, Register]),
+        AsmGenerator_TRANSFERT(Operators.GOTO,    "MOVE",  "01000#5#.3.3", operands = [Register, Register]),
+        AsmGenerator_TRANSFERT(Operators.PRINT,   "PRINT", "00100#8#.3", operands = [Register]),
+        AsmGenerator_TRANSFERT(Operators.INPUT,   "INPUT", "0010100.9",       operands = [Variable]),
+        AsmGenerator_TRANSFERT(Operators.LOAD,    "LOAD",  "0011.3.9",        operands = [Variable, Register]),
+        AsmGenerator_TRANSFERT(Operators.STORE,   "STORE", "0111.9.3",        operands = [Register, Variable]),
+        AsmGenerator_SINGLE(Operators.HALT,       "HALT",  "#16"),
+        AsmGenerator_TRANSFERT(Operators.GOTO,    "JMP",   "00001.11",      operands = [Label]),
+        AsmGenerator_CONDITIONAL_GOTO(Operators.GOTO, "CMP;BNE", "00011#5#.3.3.0001000.9", comparator = Operators.NOTEQ),
+        AsmGenerator_CONDITIONAL_GOTO(Operators.GOTO, "CMP;BEQ", "00011#5#.3.3.0001001.9", comparator = Operators.EQ),
+        AsmGenerator_CONDITIONAL_GOTO(Operators.GOTO, "CMP;BLT", "00011#5#.3.3.0001010.9", comparator = Operators.INF),
+        AsmGenerator_CONDITIONAL_GOTO(Operators.GOTO, "CMP;BGT", "00011#5#.3.3.0001011.9", comparator = Operators.SUP)
+    ]
 
-    _litteralsCommands     :Dict[Operator,Commands] = {
-         Operators.MOVE     : { "opcode":"01001", "asm":"MOVE" },
-    }
 
-    _commands              :Dict[Operator,Commands] = {
-        Operators.HALT         : { "opcode":"00000", "asm":"HALT" },
-        Operators.GOTO         : { "opcode":"00001", "asm":"JMP" },
-        Operators.NOTEQ        : { "opcode":"0001000", "asm":"BNE" },
-        Operators.EQ           : { "opcode":"0001001", "asm":"BEQ" },
-        Operators.INF          : { "opcode":"0001010", "asm":"BLT" },
-        Operators.SUP          : { "opcode":"0001011", "asm":"BGT" },
-        Operators.CMP          : { "opcode":"00011", "asm":"CMP" },
-        Operators.PRINT        : { "opcode":"00100", "asm":"PRINT" },
-        Operators.INPUT        : { "opcode":"00101", "asm":"INPUT" },
-        Operators.LOAD         : { "opcode":"0011", "asm":"LOAD" },
-        Operators.MOVE         : { "opcode":"01000", "asm":"MOVE" },
-        Operators.STORE        : { "opcode":"0111", "asm":"STORE"}
-    }
 
     # hérité
     def litteralOperatorAvailable(self, operator:Operator, litteral:Litteral) -> bool:
@@ -74,7 +117,18 @@ class Processor16Bits(ProcessorEngine):
         :rtype: bool
         """
 
-        if operator in self._litteralArithmeticsCommands:
+        if operator in (
+            Operators.NEG,
+            Operators.INVERSE,
+            Operators.ADD,
+            Operators.MINUS,
+            Operators.MULT,
+            Operators.DIV,
+            Operators.MOD,
+            Operators.AND,
+            Operators.OR,
+            Operators.XOR  
+        ):
             minVal, maxVal = self._litteralDomain
             return litteral.isBetween(minVal, maxVal)
         if operator == Operators.MOVE:
