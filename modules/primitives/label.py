80832b66ae07e2174e728f25dcd85636498697ba
gromax <gomaxx@gmail.com>
2020-07-17 15:16:53 +0200

    Possibilité d'initialisation des index

--- modules/primitives/label.py
+++ modules/primitives/label.py
@@ -7,6 +7,7 @@ class Label:
     PREFIX = "Lab"
     _currentIndex = 0 # type: int
     _name:str = ''
+    _index:int = -1
 
     @classmethod
     def getNextFreeIndex(cls) -> int:
@@ -18,11 +19,26 @@ class Label:
         cls._currentIndex += 1
         return cls._currentIndex
 
+    @classmethod
+    def initFreeIndex(cls):
+        """
+        remet à 0 le _currentIndex
+        utile en cas de compilation de plusieurs programmes à la suite
+        """
+        cls._currentIndex = 0
+
     def __init__(self):
         """Constructeur de la classe
         """
         pass
 
+    def initIndex(self):
+        """
+        remet à 0 le _currentIndex
+        utile en cas de compilation de plusieurs programmes à la suite
+        """
+        self._index = Label.getNextFreeIndex()
+    
     @property
     def name(self) -> 'str':
         """Assigne le nom si nécessaire et le retourne
@@ -30,11 +46,9 @@ class Label:
         :return: nom de l'étiquette
         :rtype: str
         """
-
-        if self._name == "":
-            index = Label.getNextFreeIndex()
-            self._name = self.PREFIX+str(index)
-        return self._name
+        if self._index == -1:
+            self._index = Label.getNextFreeIndex()
+        return self.PREFIX+str(self._index)
 
     def __str__(self) -> 'str':
         """Transtypage -> str. Affiche le nom de l'étiquette
