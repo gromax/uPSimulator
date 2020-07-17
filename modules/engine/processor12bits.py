a49b6983d4d9bdd9e0206593a1e9e09be3b5c5d3
gromax <gomaxx@gmail.com>
2020-07-17 15:12:16 +0200

    ajout des décodeurs

--- modules/engine/processor12bits.py
+++ modules/engine/processor12bits.py
@@ -3,7 +3,7 @@
    :synopsis: Exemple de modèle de processeur
 """
 
-from typing import Dict, List, Tuple
+from typing import Tuple
 
 from modules.engine.processorengine import ProcessorEngine
 from modules.primitives.operators import Operators, Operator
@@ -13,28 +13,32 @@ from modules.primitives.variable import Variable
 from modules.primitives.register import Register
 from modules.primitives.label import Label
 from modules.engine.asmgenerator import AsmGenerator ,AsmGenerator_SINGLE, AsmGenerator_TRANSFERT, AsmGenerator_CONDITIONAL_GOTO
+from modules.engine.decode import Decodeur, ArgsType
+
 
 '''
-HALT : 000000000000
-GOTO : 0001XXXXXXXX
-CMP  : 11110101XXXX
-BEQ  : 0010XXXXXXXX
-BLT  : 0011XXXXXXXX
+HALT : 0000XXXXXXXX
+GOTO : 0001########
+CMP  : 11110101####
+BEQ  : 0010########
+BLT  : 0011########
 
-PRINT   : 0100XXXXXXXX
-INPUT   : 0101XXXXXXXX
-LOAD    : 100XXXXXXXXX
-MOVE    : 11110110XXXX
-INVERSE : 11110111XXXX
-ADD     : 11111000XXXX
-MINUS   : 11111001XXXX
-MULT    : 11111010XXXX
-DIV     : 11111011XXXX
-MOD     : 11111100XXXX
-AND     : 11111101XXXX
-OR      : 11111110XXXX
-XOR     : 11111111XXXX
-STORE   : 101XXXXXXXXX
+NEG     : 11110110XX##
+INVERSE : 11110111XX##
+PRINT   : 0100XXXXXX##
+INPUT   : 0101########
+LOAD    : 100#########
+MOVE    : 11110110####
+INVERSE : 11110111####
+ADD     : 11111000####
+MINUS   : 11111001####
+MULT    : 11111010####
+DIV     : 11111011####
+MOD     : 11111100####
+AND     : 11111101####
+OR      : 11111110####
+XOR     : 11111111####
+STORE   : 101#########
 '''
 
 
@@ -51,7 +55,7 @@ class Processor12Bits(ProcessorEngine):
         Operators.INF
     ]
 
-    _asmGenerators:List[AsmGenerator] = [
+    _asmGenerators:Tuple(AsmGenerator,...) = [
         AsmGenerator_TRANSFERT(Operators.NEG,     "NEG",   "11110110#2#.0.2", operands = [Register, Register]),
         AsmGenerator_TRANSFERT(Operators.INVERSE, "NOT",   "11110111#2#.0.2", operands = [Register, Register]),
         AsmGenerator_TRANSFERT(Operators.ADD,     "ADD",   "11111000.0.2.2", operands = [Register, Register, Register]),
@@ -67,11 +71,32 @@ class Processor12Bits(ProcessorEngine):
         AsmGenerator_TRANSFERT(Operators.INPUT,   "INPUT", "0101.8", operands = [Variable]),
         AsmGenerator_TRANSFERT(Operators.LOAD,    "LOAD",  "100.2.7", operands = [Variable, Register]),
         AsmGenerator_TRANSFERT(Operators.STORE,   "STORE", "101.7.2", operands = [Register, Variable]),
-        AsmGenerator_SINGLE(Operators.HALT,       "HALT",  "#12"),
+        AsmGenerator_SINGLE(Operators.HALT,       "HALT",  "0000#8"),
         AsmGenerator_TRANSFERT(Operators.GOTO,    "JMP",   "0001.8", operands = [Label]),
         AsmGenerator_CONDITIONAL_GOTO(Operators.GOTO, "CMP;BEQ", "11110101.2.2.0010.8", comparator = Operators.EQ),
         AsmGenerator_CONDITIONAL_GOTO(Operators.GOTO, "CMP;BLT", "11110101.2.2.0011.8", comparator = Operators.INF)
     ]
 
-
+    _decodeurs: Tuple[Decodeur,...] = (
+        Decodeur("11110110XX##", Operators.NEG, (ArgsType.REGISTRE, 2)),
+        Decodeur("11110111XX##", Operators.INVERSE, (ArgsType.REGISTRE, 2)),
+        Decodeur("11111000####", Operators.ADD, (ArgsType.REGISTRE, 2), (ArgsType.REGISTRE, 2)),
+        Decodeur("11111001####", Operators.MINUS, (ArgsType.REGISTRE, 2), (ArgsType.REGISTRE, 2)),
+        Decodeur("11111010####", Operators.MULT, (ArgsType.REGISTRE, 2), (ArgsType.REGISTRE, 2)),
+        Decodeur("11111011####", Operators.DIV, (ArgsType.REGISTRE, 2), (ArgsType.REGISTRE, 2)),
+        Decodeur("11111100####", Operators.MOD, (ArgsType.REGISTRE, 2), (ArgsType.REGISTRE, 2)),
+        Decodeur("11111101####", Operators.AND, (ArgsType.REGISTRE, 2), (ArgsType.REGISTRE, 2)),
+        Decodeur("11111110####", Operators.OR, (ArgsType.REGISTRE, 2), (ArgsType.REGISTRE, 2)),
+        Decodeur("11111111####", Operators.XOR, (ArgsType.REGISTRE, 2), (ArgsType.REGISTRE, 2)),
+        Decodeur("11110110####", Operators.MOVE, (ArgsType.REGISTRE, 2), (ArgsType.REGISTRE, 2)),
+        Decodeur("0100XXXXXX##", Operators.PRINT, (ArgsType.REGISTRE, 2)),
+        Decodeur("0101########", Operators.INPUT, (ArgsType.ADRESSE, 8)),
+        Decodeur("100#########", Operators.LOAD, (ArgsType.REGISTRE, 2), (ArgsType.ADRESSE, 7)),
+        Decodeur("101#########", Operators.STORE, (ArgsType.ADRESSE, 7), (ArgsType.REGISTRE, 2)),
+        Decodeur("0000XXXXXXXX", Operators.HALT),
+        Decodeur("0001########", Operators.GOTO, (ArgsType.ADRESSE, 8)),
+        Decodeur("0010########", Operators.EQ, (ArgsType.ADRESSE, 8)),
+        Decodeur("0011########", Operators.INF, (ArgsType.ADRESSE, 8)),
+        Decodeur("11110101####", Operators.CMP, (ArgsType.REGISTRE, 2), (ArgsType.REGISTRE, 2))
+    )
 
--- modules/engine/processor16bits.py
+++ modules/engine/processor16bits.py
@@ -3,55 +3,56 @@
    :synopsis: Exemple de modèle de processeur
 """
 
-from typing import Dict, List, Tuple
+from typing import Tuple
 
 from modules.engine.processorengine import ProcessorEngine
 from modules.primitives.operators import Operators, Operator
-from modules.primitives.actionsfifo import ActionType
+from modules.primitives.actionsfifo import ActionType, ActionsFIFO
 from modules.primitives.litteral import Litteral
 from modules.primitives.variable import Variable
 from modules.primitives.register import Register
 from modules.primitives.label import Label
 from modules.engine.asmgenerator import AsmGenerator ,AsmGenerator_SINGLE, AsmGenerator_TRANSFERT, AsmGenerator_CONDITIONAL_GOTO
 
+from modules.engine.decode import Decodeur, ArgsType
 
 '''
 littéraux
-NEG     : 010110XXXXXXXXXX
-INVERSE : 010111XXXXXXXXXX
-ADD     : 1000XXXXXXXXXXXX
-MINUS   : 1001XXXXXXXXXXXX
-MULT    : 1010XXXXXXXXXXXX
-DIV     : 1011XXXXXXXXXXXX
-MOD     : 1100XXXXXXXXXXXX
-AND     : 1101XXXXXXXXXXXX
-OR      : 1110XXXXXXXXXXXX
-XOR     : 1111XXXXXXXXXXXX
-MOVE    : 01001XXXXXXXXXXX
+NEG     : 010110##########
+INVERSE : 010111##########
+ADD     : 1000############
+MINUS   : 1001############
+MULT    : 1010############
+DIV     : 1011############
+MOD     : 1100############
+AND     : 1101############
+OR      : 1110############
+XOR     : 1111############
+MOVE    : 01001###########
 autres
 HALT    : 0000XXXXXXXXXXXX
-NEG     : 010100XXXXXXXXXX
-INVERSE : 010101XXXXXXXXXX
-ADD     : 0110000XXXXXXXXX
-MINUS   : 0110001XXXXXXXXX
-MULT    : 0110010XXXXXXXXX
-DIV     : 0110011XXXXXXXXX
-MOD     : 0110100XXXXXXXXX
-AND     : 0110101XXXXXXXXX
-OR      : 0110110XXXXXXXXX
-XOR     : 0110111XXXXXXXXX
+NEG     : 010100XXXX######
+INVERSE : 010101XXXX######
+ADD     : 0110000#########
+MINUS   : 0110001#########
+MULT    : 0110010#########
+DIV     : 0110011#########
+MOD     : 0110100#########
+AND     : 0110101#########
+OR      : 0110110#########
+XOR     : 0110111#########
 HALT    : 00000XXXXXXXXXXX
-GOTO    : 00001XXXXXXXXXXX
-NOTEQ   : 0001000XXXXXXXXX
-EQ      : 0001001XXXXXXXXX
-INF     : 0001010XXXXXXXXX
-SUP     : 0001011XXXXXXXXX
-CMP     : 00011XXXXXXXXXXX
-PRINT   : 00100XXXXXXXXXXX
-INPUT   : 00101XXXXXXXXXXX
-LOAD    : 0011XXXXXXXXXXXX
-MOVE    : 01000XXXXXXXXXXX
-STORE   : 0111XXXXXXXXXXXX
+GOTO    : 00001###########
+NOTEQ   : 0001000#########
+EQ      : 0001001#########
+INF     : 0001010#########
+SUP     : 0001011#########
+CMP     : 00011XXXXX######
+PRINT   : 00100XXXXXXXX###
+INPUT   : 00101XX#########
+LOAD    : 0011############
+MOVE    : 01000XXXXX######
+STORE   : 0111############
 '''
 
 class Processor16Bits(ProcessorEngine):
@@ -68,7 +69,7 @@ class Processor16Bits(ProcessorEngine):
         Operators.SUP
     ]
 
-    _asmGenerators: List[AsmGenerator] = [
+    _asmGenerators: Tuple[AsmGenerator, ...] = (
         AsmGenerator_TRANSFERT(Operators.NEG,     "NEG",   "010110.3.7",     operands = [Litteral, Register]),
         AsmGenerator_TRANSFERT(Operators.INVERSE, "NOT",   "010111.3.7",     operands = [Litteral, Register]),
         AsmGenerator_TRANSFERT(Operators.ADD,     "ADD",   "1000.3.3.6",     operands = [Register, Litteral, Register]),
@@ -80,8 +81,8 @@ class Processor16Bits(ProcessorEngine):
         AsmGenerator_TRANSFERT(Operators.OR,      "OR",    "1110.3.3.6",     operands = [Register, Litteral, Register]),
         AsmGenerator_TRANSFERT(Operators.XOR,     "XOR",   "1111.3.3.6",     operands = [Register, Litteral, Register]),
         AsmGenerator_TRANSFERT(Operators.MOVE,    "MOVE",  "01001.3.8",      operands = [Litteral, Register]),
-        AsmGenerator_TRANSFERT(Operators.NEG,     "NEG",   "010100#4#.3.3", operands = [Register, Register]),
-        AsmGenerator_TRANSFERT(Operators.INVERSE, "NOT",   "010101#4#.3.3", operands = [Register, Register]),
+        AsmGenerator_TRANSFERT(Operators.NEG,     "NEG",   "010100#4#.3.3",  operands = [Register, Register]),
+        AsmGenerator_TRANSFERT(Operators.INVERSE, "NOT",   "010101#4#.3.3",  operands = [Register, Register]),
         AsmGenerator_TRANSFERT(Operators.ADD,     "ADD",   "0110000.3.3.3",  operands = [Register, Register, Register]),
         AsmGenerator_TRANSFERT(Operators.MINUS,   "SUB",   "0110001.3.3.3",  operands = [Register, Register, Register]),
         AsmGenerator_TRANSFERT(Operators.MULT,    "MULT",  "0110010.3.3.3",  operands = [Register, Register, Register]),
@@ -90,19 +91,54 @@ class Processor16Bits(ProcessorEngine):
         AsmGenerator_TRANSFERT(Operators.AND,     "AND",   "0110101.3.3.3",  operands = [Register, Register, Register]),
         AsmGenerator_TRANSFERT(Operators.OR,      "OR",    "0110110.3.3.3",  operands = [Register, Register, Register]),
         AsmGenerator_TRANSFERT(Operators.XOR,     "XOR",   "0110111.3.3.3",  operands = [Register, Register, Register]),
-        AsmGenerator_TRANSFERT(Operators.GOTO,    "MOVE",  "01000#5#.3.3", operands = [Register, Register]),
-        AsmGenerator_TRANSFERT(Operators.PRINT,   "PRINT", "00100#8#.3", operands = [Register]),
-        AsmGenerator_TRANSFERT(Operators.INPUT,   "INPUT", "0010100.9",       operands = [Variable]),
-        AsmGenerator_TRANSFERT(Operators.LOAD,    "LOAD",  "0011.3.9",        operands = [Variable, Register]),
-        AsmGenerator_TRANSFERT(Operators.STORE,   "STORE", "0111.9.3",        operands = [Register, Variable]),
-        AsmGenerator_SINGLE(Operators.HALT,       "HALT",  "#16"),
-        AsmGenerator_TRANSFERT(Operators.GOTO,    "JMP",   "00001.11",      operands = [Label]),
+        AsmGenerator_TRANSFERT(Operators.GOTO,    "MOVE",  "01000#5#.3.3",   operands = [Register, Register]),
+        AsmGenerator_TRANSFERT(Operators.PRINT,   "PRINT", "00100#8#.3",     operands = [Register]),
+        AsmGenerator_TRANSFERT(Operators.INPUT,   "INPUT", "00101#2#.9",      operands = [Variable]),
+        AsmGenerator_TRANSFERT(Operators.LOAD,    "LOAD",  "0011.3.9",       operands = [Variable, Register]),
+        AsmGenerator_TRANSFERT(Operators.STORE,   "STORE", "0111.9.3",       operands = [Register, Variable]),
+        AsmGenerator_SINGLE(Operators.HALT,       "HALT",  "0000#12"),
+        AsmGenerator_TRANSFERT(Operators.GOTO,    "JMP",   "00001.11",       operands = [Label]),
         AsmGenerator_CONDITIONAL_GOTO(Operators.GOTO, "CMP;BNE", "00011#5#.3.3.0001000.9", comparator = Operators.NOTEQ),
         AsmGenerator_CONDITIONAL_GOTO(Operators.GOTO, "CMP;BEQ", "00011#5#.3.3.0001001.9", comparator = Operators.EQ),
         AsmGenerator_CONDITIONAL_GOTO(Operators.GOTO, "CMP;BLT", "00011#5#.3.3.0001010.9", comparator = Operators.INF),
         AsmGenerator_CONDITIONAL_GOTO(Operators.GOTO, "CMP;BGT", "00011#5#.3.3.0001011.9", comparator = Operators.SUP)
-    ]
+    )
 
+    _decodeurs: Tuple[Decodeur,...] = (
+        Decodeur("010110##########", Operators.NEG, (ArgsType.REGISTRE, 3), (ArgsType.LITTERAL, 7)),
+        Decodeur("010111##########", Operators.INVERSE, (ArgsType.REGISTRE, 3), (ArgsType.LITTERAL, 7)),
+        Decodeur("1000############", Operators.ADD, (ArgsType.REGISTRE, 3), (ArgsType.REGISTRE, 3), (ArgsType.LITTERAL, 6)),
+        Decodeur("1001############", Operators.MINUS, (ArgsType.REGISTRE, 3), (ArgsType.REGISTRE, 3), (ArgsType.LITTERAL, 6)),
+        Decodeur("1010############", Operators.MULT, (ArgsType.REGISTRE, 3), (ArgsType.REGISTRE, 3), (ArgsType.LITTERAL, 6)),
+        Decodeur("1011############", Operators.DIV, (ArgsType.REGISTRE, 3), (ArgsType.REGISTRE, 3), (ArgsType.LITTERAL, 6)),
+        Decodeur("1100############", Operators.MOD, (ArgsType.REGISTRE, 3), (ArgsType.REGISTRE, 3), (ArgsType.LITTERAL, 6)),
+        Decodeur("1101############", Operators.AND, (ArgsType.REGISTRE, 3), (ArgsType.REGISTRE, 3), (ArgsType.LITTERAL, 6)),
+        Decodeur("1110############", Operators.OR, (ArgsType.REGISTRE, 3), (ArgsType.REGISTRE, 3), (ArgsType.LITTERAL, 6)),
+        Decodeur("1111############", Operators.XOR, (ArgsType.REGISTRE, 3), (ArgsType.REGISTRE, 3), (ArgsType.LITTERAL, 6)),
+        Decodeur("01001###########", Operators.MOVE, (ArgsType.REGISTRE, 3), (ArgsType.LITTERAL, 8)),
+        Decodeur("010100XXXX######", Operators.NEG, (ArgsType.REGISTRE, 3), (ArgsType.REGISTRE, 3)),
+        Decodeur("010101XXXX######", Operators.INVERSE, (ArgsType.REGISTRE, 3), (ArgsType.REGISTRE, 3)),
+        Decodeur("0110000#########", Operators.ADD, (ArgsType.REGISTRE, 3), (ArgsType.REGISTRE, 3), (ArgsType.REGISTRE, 3)),
+        Decodeur("0110001#########", Operators.MINUS, (ArgsType.REGISTRE, 3), (ArgsType.REGISTRE, 3), (ArgsType.REGISTRE, 3)),
+        Decodeur("0110010#########", Operators.MULT, (ArgsType.REGISTRE, 3), (ArgsType.REGISTRE, 3), (ArgsType.REGISTRE, 3)),
+        Decodeur("0110011#########", Operators.DIV, (ArgsType.REGISTRE, 3), (ArgsType.REGISTRE, 3), (ArgsType.REGISTRE, 3)),
+        Decodeur("0110100#########", Operators.MOD, (ArgsType.REGISTRE, 3), (ArgsType.REGISTRE, 3), (ArgsType.REGISTRE, 3)),
+        Decodeur("0110101#########", Operators.AND, (ArgsType.REGISTRE, 3), (ArgsType.REGISTRE, 3), (ArgsType.REGISTRE, 3)),
+        Decodeur("0110110#########", Operators.OR, (ArgsType.REGISTRE, 3), (ArgsType.REGISTRE, 3), (ArgsType.REGISTRE, 3)),
+        Decodeur("0110111#########", Operators.XOR, (ArgsType.REGISTRE, 3), (ArgsType.REGISTRE, 3), (ArgsType.REGISTRE, 3)),
+        Decodeur("01000XXXXX######", Operators.MOVE, (ArgsType.REGISTRE, 3), (ArgsType.REGISTRE, 3)),
+        Decodeur("00100XXXXXXXX###", Operators.PRINT, (ArgsType.REGISTRE, 3)),
+        Decodeur("00101XX#########", Operators.INPUT, (ArgsType.ADRESSE, 9)),
+        Decodeur("0011############", Operators.LOAD, (ArgsType.REGISTRE, 3), (ArgsType.ADRESSE, 9)),
+        Decodeur("0111############", Operators.STORE, (ArgsType.ADRESSE, 9), (ArgsType.REGISTRE, 3)),
+        Decodeur("0000XXXXXXXXXXXX", Operators.HALT),
+        Decodeur("00001###########", Operators.GOTO, (ArgsType.ADRESSE, 11)),
+        Decodeur("0001000#########", Operators.NOTEQ, (ArgsType.ADRESSE, 9)),
+        Decodeur("0001001#########", Operators.EQ, (ArgsType.ADRESSE, 9)),
+        Decodeur("0001010#########", Operators.INF, (ArgsType.ADRESSE, 9)),
+        Decodeur("0001011#########", Operators.SUP, (ArgsType.ADRESSE, 9)),
+        Decodeur("0011XXXXX#######", Operators.CMP, (ArgsType.REGISTRE, 3), (ArgsType.REGISTRE, 3))
+    )
 
 
     # hérité
@@ -134,5 +170,4 @@ class Processor16Bits(ProcessorEngine):
         if operator == Operators.MOVE:
             return litteral.isBetween(0, 255)
         return False
-        
         
